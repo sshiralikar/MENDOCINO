104c7df3003a39cf0cd6181fd5b5016ac5d934e9
sshiralikar <sshiralikar@trustvip.com>
2023-05-15 21:26:41 -0400

    INCLUDES_CUSTOM updates

--- INCLUDES_CUSTOM/loadASITable.js
+++ INCLUDES_CUSTOM/loadASITable.js
@@ -56,19 +56,3 @@ function loadASITable(tname) {
     }
     return tempArray;
 }
-
-function getCapFileDate(){
-    var itemCap = capId;
-    if (arguments.length > 0) {
-        itemCap = arguments[0]; // use cap ID specified in args
-    }
-
-    if(itemCap){
-        var c = aa.cap.getCap(itemCap).getOutput();
-        if(c){
-            var d = c.getFileDate();
-            return "" + d.getMonth() + "/" + d.getDayOfMonth() + "/" + d.getYear();
-        }
-    }
-    return null;
-}
