39ff2fe4dfaf40cc1bf324c0ed3c2eb3f5858abe
sshiralikar <sshiralikar@trustvip.com>
2023-05-18 10:44:54 -0400

    CAMEND-313

--- EVENT/WTUA;CANNABIS!~!APPLICATION!NA.js
+++ EVENT/WTUA;CANNABIS!~!APPLICATION!NA.js
@@ -116,54 +116,115 @@ if(wfStatus == "Deficiency")
 //CAMEND-303
 if(wfTask == "Supervisor Review" && wfStatus == "Denied")
 {
-    var VRFiles = null;
-    var rParams = aa.util.newHashMap();
-    rParams.put("RecordID", capId.getCustomID()+"");
-    logDebug("Report parameter RecordID set to: "+ capId.getCustomID()+"");
-    var report = aa.reportManager.getReportInfoModelByName("Cannabis Denial Decision Letter");
-    report = report.getOutput();
-    report.setModule("Cannabis");
-    report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
-    report.setReportParameters(rParams);
-    report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
+    var date = getCapFileDate(capId);
+    if(isDateInRangeToOct(date))
+    {
+        var VRFiles = null;
+        var rParams = aa.util.newHashMap();
+        rParams.put("RecordID", capId.getCustomID()+"");
+        logDebug("Report parameter RecordID set to: "+ capId.getCustomID()+"");
+        var report = aa.reportManager.getReportInfoModelByName("Cannabis Deficiency Denial Pending Letter");
+        report = report.getOutput();
+        report.setModule("Cannabis");
+        report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
+        report.setReportParameters(rParams);
+        report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
 
 
-    var permit = aa.reportManager.hasPermission("Cannabis Denial Decision Letter",currentUserID);
+        var permit = aa.reportManager.hasPermission("Cannabis Deficiency Denial Pending Letter",currentUserID);
 
-    if (permit.getOutput().booleanValue()) {
-        logDebug("User has Permission to run the report....");
-        var reportResult = aa.reportManager.getReportResult(report);
-        if(reportResult) {
-            reportOutput = reportResult.getOutput();
-            var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
-            logDebug("Report Run Successfull:"+ reportFile.getSuccess());
-            reportFile=reportFile.getOutput();
-            VRFiles.push(reportFile);
+        if (permit.getOutput().booleanValue()) {
+            logDebug("User has Permission to run the report....");
+            var reportResult = aa.reportManager.getReportResult(report);
+            if(reportResult) {
+                reportOutput = reportResult.getOutput();
+                var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
+                logDebug("Report Run Successfull:"+ reportFile.getSuccess());
+                reportFile=reportFile.getOutput();
+                VRFiles.push(reportFile);
+            }
+        }
+
+        var hm = new Array();
+        var conName = "";
+        var contactResult = aa.people.getCapContactByCapID(capId);
+        if (contactResult.getSuccess()) {
+            var capContacts = contactResult.getOutput();
+            for (var i in capContacts) {
+                conName = getContactName(capContacts[i]);
+                var params = aa.util.newHashtable();
+                addParameter(params, "$$altID$$", capId.getCustomID()+"");
+                addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias()+"");
+                addParameter(params, "$$capName$$", capName);
+                addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptName"));
+                addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
+                addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
+                addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
+                addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
+                addParameter(params, "$$contactname$$", conName);
+                addParameter(params, "$$date$$", sysDateMMDDYYYY);
+                addParameter(params, "$$contactEmail$$", capContacts[i].getPeople().getEmail() + "");
+                addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
+                addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
+                if(hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
+
+                    sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "CAN_DEFICIENCY", params, null, capId);
+                    hm[capContacts[i].getPeople().getEmail() + ""] = 1;
+                }
+            }
         }
     }
+    else
+    {
+        var VRFiles = null;
+        var rParams = aa.util.newHashMap();
+        rParams.put("RecordID", capId.getCustomID()+"");
+        logDebug("Report parameter RecordID set to: "+ capId.getCustomID()+"");
+        var report = aa.reportManager.getReportInfoModelByName("Cannabis Denial Decision Letter");
+        report = report.getOutput();
+        report.setModule("Cannabis");
+        report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
+        report.setReportParameters(rParams);
+        report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
 
-    var hm = new Array();
-    var conName = "";
-    var contactResult = aa.people.getCapContactByCapID(capId);
-    if (contactResult.getSuccess()) {
-        var capContacts = contactResult.getOutput();
-        for (var i in capContacts) {
-            conName = getContactName(capContacts[i]);
-            var params = aa.util.newHashtable();
-            addParameter(params, "$$altID$$", capId.getCustomID()+"");
-            addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias()+"");
-            addParameter(params, "$$capName$$", capName);
-            addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptName"));
-            addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
-            addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
-            addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
-            addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
-            addParameter(params, "$$contactname$$", conName);
-            addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
-            addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
-            if(hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
-                sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "GLOBAL_DENIED", params, VRFiles, capId);
-                hm[capContacts[i].getPeople().getEmail() + ""] = 1;
+
+        var permit = aa.reportManager.hasPermission("Cannabis Denial Decision Letter",currentUserID);
+
+        if (permit.getOutput().booleanValue()) {
+            logDebug("User has Permission to run the report....");
+            var reportResult = aa.reportManager.getReportResult(report);
+            if(reportResult) {
+                reportOutput = reportResult.getOutput();
+                var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
+                logDebug("Report Run Successfull:"+ reportFile.getSuccess());
+                reportFile=reportFile.getOutput();
+                VRFiles.push(reportFile);
+            }
+        }
+
+        var hm = new Array();
+        var conName = "";
+        var contactResult = aa.people.getCapContactByCapID(capId);
+        if (contactResult.getSuccess()) {
+            var capContacts = contactResult.getOutput();
+            for (var i in capContacts) {
+                conName = getContactName(capContacts[i]);
+                var params = aa.util.newHashtable();
+                addParameter(params, "$$altID$$", capId.getCustomID()+"");
+                addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias()+"");
+                addParameter(params, "$$capName$$", capName);
+                addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptName"));
+                addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
+                addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
+                addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
+                addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
+                addParameter(params, "$$contactname$$", conName);
+                addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
+                addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
+                if(hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
+                    sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "GLOBAL_DENIED", params, VRFiles, capId);
+                    hm[capContacts[i].getPeople().getEmail() + ""] = 1;
+                }
             }
         }
     }
