fdee380858755cefdc2bfb52c78cf2234e1cfc67
sshiralikar <sshiralikar@trustvip.com>
2023-05-18 13:33:43 -0400

    Changes

--- BATCH/BATCH_PERMIT_ABOUT_TO_EXPIRE.js
+++ BATCH/BATCH_PERMIT_ABOUT_TO_EXPIRE.js
@@ -282,12 +282,12 @@ function mainProcess() {
                     var perfFlag = getAppSpecific("Performance Standard Violation",capId);
                     if(perfFlag == "CHECKED" && appStatus != "Modification Under Review")
                     {
-                        var pCapId = getParent(capId);
-                        if(pCapId)
-                        {
-                            updateAppStatus("Modification Required","",pCapId);
+                        //var pCapId = getParent(capId);
+                        //if(pCapId)
+                        //{
+                            updateAppStatus("Modification Required","",capId);
                             setLicExpirationDate(capId,"","","About to Expire");
-                        }
+                        //}
                     }
                     else if(appStatus != "Modification Under Review")
                     {
