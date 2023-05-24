d6338752e061fbb7e0cb1ae808b2152d3033834e
sshiralikar <sshiralikar@trustvip.com>
2023-05-24 15:23:50 -0400

    Sequence Mask Changes

--- INCLUDES_CUSTOM/setRecordAltID.js
+++ INCLUDES_CUSTOM/setRecordAltID.js
@@ -11,14 +11,22 @@ function setRecordAltID(vCapID)
     }
     else if(level2 == "Amendment" || level3 == "Renewal")
     {
-
-        var parentCapId = getParent(vCapID);
-        if (parentCapId == null || parentCapId == false) {
-            parentCapId = aa.env.getValue("ParentCapID");
+        var parentCapId = aa.env.getValue("ParentCapID");
+        if (parentCapId == null || parentCapId == false || parentCapId == "") {
+            parentCapId = getParent(vCapID);
         }
-        newAltId = String(parentCapId.getCustomID()).split("-APP")[0];
+        parentCapId = aa.cap.getCapID(parentCapId.getID1(),parentCapId.getID2(),parentCapId.getID3()).getOutput();
+        //aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "parentCapId", parentCapId);
+        var vPCapType = aa.cap.getCap(parentCapId).getOutput().getCapType().toString();
+        if(vPCapType == "Cannabis/Nursery/Application/NA" || vPCapType == "Cannabis/Cultivation/Application/NA")
+            newAltId = String(parentCapId.getCustomID()).split("-APP")[0];
+        else if(vPCapType == "Cannabis/Nursery/Renewal/NA" || vPCapType == "Cannabis/Cultivation/Renewal/NA")
+            newAltId = String(parentCapId.getCustomID()).split("-REN")[0];
+        else
+            newAltId = parentCapId.getCustomID()+"";
+        //aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "newAltId", newAltId);
         var cIds = getChildren(vCapType,parentCapId);
-        var recCnt = -1;
+        var recCnt = 0;
         for(var x in cIds) {
             var recId = "" + cIds[x];
             if(recId.substring(2,5) != "EST") {
@@ -50,13 +58,17 @@ function setRecordAltID(vCapID)
             newAltId = newAltId+"-REN-"+ amendNbr;
         else if(vCapType == "Cannabis/Nursery/Renewal/NA")
             newAltId = newAltId+"-REN-"+ amendNbr;
+
+        //aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "FINALnewAltId", newAltId);
     }
     if(newAltId!="")
     {
         var updateResult = aa.cap.updateCapAltID(vCapID, newAltId);
         if(!updateResult.getSuccess()){
+            //aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "UNSUCCESSFULL", updateResult.getErrorMessage());
             aa.print("Error updating Alt Id: " + newAltId + ":: " +updateResult.getErrorMessage());
         }else{
+            //aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "SUCCESS", "SUCCESS");
             aa.print("Compliance Method record ID updated to : " + newAltId);
         }
     }
