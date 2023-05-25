8d204b7c59ca46f415d260cf3d55992280a0105e
sshiralikar <sshiralikar@trustvip.com>
2023-05-24 21:39:27 -0400

    Issuance Email

--- SCRIPTS/RUN_ASYNC_SEND_ISSUANCE_EMAIL.js
+++ SCRIPTS/RUN_ASYNC_SEND_ISSUANCE_EMAIL.js
@@ -63,6 +63,26 @@ try
             }
         }
     }
+    function getContactName(vConObj) {
+        if (vConObj.people.getContactTypeFlag() == "organization") {
+            if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+                return vConObj.people.getBusinessName();
+
+            return vConObj.people.getBusinessName2();
+        }
+        else {
+            if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "") {
+                return vConObj.people.getFullName();
+            }
+            if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null) {
+                return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
+            }
+            if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+                return vConObj.people.getBusinessName();
+
+            return vConObj.people.getBusinessName2();
+        }
+    }
     function addParameter(pamaremeters, key, value)
     {
         if(key != null)
