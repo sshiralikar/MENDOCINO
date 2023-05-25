d12ea131be77887f0d833053ef1b1f9f6d4f472b
sshiralikar <sshiralikar@trustvip.com>
2023-05-24 21:45:31 -0400

    Issuance Email

--- SCRIPTS/RUN_ASYNC_SEND_ISSUANCE_EMAIL.js
+++ SCRIPTS/RUN_ASYNC_SEND_ISSUANCE_EMAIL.js
@@ -4,8 +4,12 @@ try
     var hm = new Array();
     var rParams = aa.util.newHashMap();
     currentUserID = "ADMIN";
+    var sysDate = aa.date.getCurrentDate();
+    var sysDateMMDDYYYY = dateFormatted(sysDate.getMonth(),sysDate.getDayOfMonth(),sysDate.getYear(),"");
+    var capName = cap.getSpecialText();
     var licCapId = aa.cap.getCapID(aa.env.getValue("RecordID")).getOutput();
     var capId = aa.cap.getCapID(aa.env.getValue("capId")).getOutput();
+    var cap = aa.cap.getCap(capId).getOutput();
     rParams.put("RecordID", aa.env.getValue("RecordID"));
 
 
@@ -33,7 +37,8 @@ try
     if (contactResult.getSuccess()) {
         var capContacts = contactResult.getOutput();
         for (var i in capContacts) {
-            if(capContacts[i].getPeople().getContactType() == "Applicant" || capContacts[i].getPeople().getContactType() == "Authorized Agent")
+            if(capContacts[i].getPeople().getContactType() == "Applicant"
+                || capContacts[i].getPeople().getContactType() == "Authorized Agent")
             {
                 conName = getContactName(capContacts[i]);
 
@@ -63,101 +68,123 @@ try
             }
         }
     }
-    function getContactName(vConObj) {
-        if (vConObj.people.getContactTypeFlag() == "organization") {
-            if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
-                return vConObj.people.getBusinessName();
-
-            return vConObj.people.getBusinessName2();
-        }
-        else {
-            if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "") {
-                return vConObj.people.getFullName();
-            }
-            if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null) {
-                return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
-            }
-            if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
-                return vConObj.people.getBusinessName();
-
-            return vConObj.people.getBusinessName2();
-        }
-    }
-    function addParameter(pamaremeters, key, value)
-    {
-        if(key != null)
-        {
-            if(value == null)
-            {
-                value = "";
-            }
-
-            pamaremeters.put(key, value);
-        }
-    }
-    function sendEmail(fromEmail, toEmail, CC, template, eParams, files) { // optional: itemCap
-        var itemCap = capId;
-        if (arguments.length == 7)
-            itemCap = arguments[6]; // use cap ID specified in args
-
-        //var sent = aa.document.sendEmailByTemplateName(fromEmail, toEmail, CC, template, eParams, files);
-        if(eParams)
-        {
-            var bizDomScriptResult = aa.bizDomain.getBizDomain("NOTIFICATION_TEMPLATE_INFO_CANNABIS");
-            if (bizDomScriptResult.getSuccess()) {
-                var bizDomScriptArray = bizDomScriptResult.getOutput().toArray()
-                for (var i in bizDomScriptArray) {
-                    var desc = bizDomScriptArray[i].getDescription();
-                    var value = bizDomScriptArray[i].getBizdomainValue() + "";
-                    addParameter(eParams,value,desc);
-                }
-            }
-        }
-        toEmail = runEmailThroughSLEmailFilter(toEmail);
-        var itempAltIDScriptModel = aa.cap.createCapIDScriptModel(itemCap.getID1(), itemCap.getID2(), itemCap.getID3());
-        var sent = aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, CC, template, eParams, itempAltIDScriptModel, files);
-    }
-    function runEmailThroughSLEmailFilter(vEmail)
-    {
-        var filter = lookup("SL_EMAIL_CONTROL", "FILTER");
-        if(filter == "ON")
-        {
-            var domains = String(lookup("SL_EMAIL_CONTROL", "DOMAIN_EXCEPTIONS"));
-            var emails = String(lookup("SL_EMAIL_CONTROL", "EMAIL_EXCEPTIONS"));
-            var vOriginalDomain = vEmail.substring(vEmail.indexOf("@") + 1, vEmail.length).toLowerCase();
-
-            if(domains.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
-                return vEmail;
-            if(emails.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
-                return vEmail;
-
-
-            vEmail = vEmail.replace(vOriginalDomain, "DoNotSend.com");
-        }
-        return vEmail;
-    }
-    function lookup(stdChoice,stdValue)
-    {
-        var strControl;
-        var bizDomScriptResult = aa.bizDomain.getBizDomainByValue(stdChoice,stdValue);
-
-        if (bizDomScriptResult.getSuccess())
-        {
-            var bizDomScriptObj = bizDomScriptResult.getOutput();
-            strControl = "" + bizDomScriptObj.getDescription(); // had to do this or it bombs.  who knows why?
-        }
-        return strControl;
-    }
-    function matches(eVal, argList) {
-        for (var i = 1; i < arguments.length; i++) {
-            if (arguments[i] == eVal) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
 catch(err)
 {
     aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "Error on Issuance Email ASYNC", err);
+}
+function getContactName(vConObj) {
+    if (vConObj.people.getContactTypeFlag() == "organization") {
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+    else {
+        if (vConObj.people.getFullName() != null && vConObj.people.getFullName() != "") {
+            return vConObj.people.getFullName();
+        }
+        if (vConObj.people.getFirstName() != null && vConObj.people.getLastName() != null) {
+            return vConObj.people.getFirstName() + " " + vConObj.people.getLastName();
+        }
+        if (vConObj.people.getBusinessName() != null && vConObj.people.getBusinessName() != "")
+            return vConObj.people.getBusinessName();
+
+        return vConObj.people.getBusinessName2();
+    }
+}
+function addParameter(pamaremeters, key, value)
+{
+    if(key != null)
+    {
+        if(value == null)
+        {
+            value = "";
+        }
+
+        pamaremeters.put(key, value);
+    }
+}
+function sendEmail(fromEmail, toEmail, CC, template, eParams, files) { // optional: itemCap
+    var itemCap = capId;
+    if (arguments.length == 7)
+        itemCap = arguments[6]; // use cap ID specified in args
+
+    //var sent = aa.document.sendEmailByTemplateName(fromEmail, toEmail, CC, template, eParams, files);
+    if(eParams)
+    {
+        var bizDomScriptResult = aa.bizDomain.getBizDomain("NOTIFICATION_TEMPLATE_INFO_CANNABIS");
+        if (bizDomScriptResult.getSuccess()) {
+            var bizDomScriptArray = bizDomScriptResult.getOutput().toArray()
+            for (var i in bizDomScriptArray) {
+                var desc = bizDomScriptArray[i].getDescription();
+                var value = bizDomScriptArray[i].getBizdomainValue() + "";
+                addParameter(eParams,value,desc);
+            }
+        }
+    }
+    toEmail = runEmailThroughSLEmailFilter(toEmail);
+    var itempAltIDScriptModel = aa.cap.createCapIDScriptModel(itemCap.getID1(), itemCap.getID2(), itemCap.getID3());
+    var sent = aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, CC, template, eParams, itempAltIDScriptModel, files);
+}
+function runEmailThroughSLEmailFilter(vEmail)
+{
+    var filter = lookup("SL_EMAIL_CONTROL", "FILTER");
+    if(filter == "ON")
+    {
+        var domains = String(lookup("SL_EMAIL_CONTROL", "DOMAIN_EXCEPTIONS"));
+        var emails = String(lookup("SL_EMAIL_CONTROL", "EMAIL_EXCEPTIONS"));
+        var vOriginalDomain = vEmail.substring(vEmail.indexOf("@") + 1, vEmail.length).toLowerCase();
+
+        if(domains.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
+            return vEmail;
+        if(emails.toLowerCase().indexOf(String(vOriginalDomain).toLowerCase()) != -1)
+            return vEmail;
+
+
+        vEmail = vEmail.replace(vOriginalDomain, "DoNotSend.com");
+    }
+    return vEmail;
+}
+function lookup(stdChoice,stdValue)
+{
+    var strControl;
+    var bizDomScriptResult = aa.bizDomain.getBizDomainByValue(stdChoice,stdValue);
+
+    if (bizDomScriptResult.getSuccess())
+    {
+        var bizDomScriptObj = bizDomScriptResult.getOutput();
+        strControl = "" + bizDomScriptObj.getDescription(); // had to do this or it bombs.  who knows why?
+    }
+    return strControl;
+}
+function matches(eVal, argList) {
+    for (var i = 1; i < arguments.length; i++) {
+        if (arguments[i] == eVal) {
+            return true;
+        }
+    }
+    return false;
+}
+function dateFormatted(pMonth, pDay, pYear, pFormat)
+{
+    var mth = "";
+    var day = "";
+    var ret = "";
+    if (pMonth > 9)
+        mth = pMonth.toString();
+    else
+        mth = "0" + pMonth.toString();
+
+    if (pDay > 9)
+        day = pDay.toString();
+    else
+        day = "0" + pDay.toString();
+
+    if (pFormat == "YYYY-MM-DD")
+        ret = pYear.toString() + "-" + mth + "-" + day;
+    else
+        ret = "" + mth + "/" + day + "/" + pYear.toString();
+
+    return ret;
 }
