2f074e27e8fb001d91b4847ec8f0405520c4d1ff
sshiralikar <sshiralikar@trustvip.com>
2023-05-31 13:39:31 -0400

    Modification Changes

--- EVENT/WTUA;CANNABIS!AMENDMENT!MODIFICATION!NA.js
+++ EVENT/WTUA;CANNABIS!AMENDMENT!MODIFICATION!NA.js
@@ -439,6 +439,39 @@ function copyASITablesWithRemove(pFromCapId, pToCapId) {
         logDebug("ASI Table Array : " + tn + " (" + numrows + " Rows)");
     }
 }
+function copyAppSpecificInfo(srcCapId, targetCapId) {
+    //1. Get Application Specific Information with source CAPID.
+    var appSpecificInfo = getAppSpecificInfo(srcCapId);
+    if (appSpecificInfo == null || appSpecificInfo.length == 0) {
+        return;
+    }
+    //2. Set target CAPID to source Specific Information.
+    for (loopk in appSpecificInfo) {
+        var sourceAppSpecificInfoModel = appSpecificInfo[loopk];
+        sourceAppSpecificInfoModel.setPermitID1(targetCapId.getID1());
+        sourceAppSpecificInfoModel.setPermitID2(targetCapId.getID2());
+        sourceAppSpecificInfoModel.setPermitID3(targetCapId.getID3());
+        //3. Edit ASI on target CAP (Copy info from source to target)
+        aa.appSpecificInfo.editAppSpecInfoValue(sourceAppSpecificInfoModel);
+    }
+}
+
+function getAppSpecificInfo(capId) {
+    capAppSpecificInfo = null;
+    var s_result = aa.appSpecificInfo.getByCapID(capId);
+    if (s_result.getSuccess()) {
+        capAppSpecificInfo = s_result.getOutput();
+        if (capAppSpecificInfo == null || capAppSpecificInfo.length == 0) {
+            aa.print("WARNING: no appSpecificInfo on this CAP:" + capId);
+            capAppSpecificInfo = null;
+        }
+    } else {
+        aa.print("ERROR: Failed to appSpecificInfo: " + s_result.getErrorMessage());
+        capAppSpecificInfo = null;
+    }
+    // Return AppSpecificInfoModel[]
+    return capAppSpecificInfo;
+}
 
 
 
