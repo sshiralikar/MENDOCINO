5df9375041ae9858bbb1fe781a96873f8c55a15f
sshiralikar <sshiralikar@trustvip.com>
2023-06-13 19:18:24 -0400

    Trish's issue.

--- EVENT/ASA;CANNABIS!~!~!~.js
+++ EVENT/ASA;CANNABIS!~!~!~.js
@@ -1,3 +1,24 @@
+try
+{
+    if(!publicUser)
+    {
+        setRecordAltID(capId);
+        if (parentCapId == "undefined" || parentCapId == null) {
+            parentCapId = aa.env.getValue("ParentCapID");
+            if(!parentCapId)
+                parentCapId = getParent();
+        }
+        if(parentCapId && (appMatch("Cannabis/*/Renewal/*") || appMatch("Cannabis/Amendment/*/*")))
+            updateShortNotes(getShortNotes(parentCapId));
+        else
+            updateShortNotes("PH3");
+    }
+}
+catch (err)
+{
+    aa.print("Error on changing sequence ASA: "+ err);
+    aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "Error on changing sequence ASA", err);
+}
 if(!publicUser && (appTypeArray[1]=="Amendment" || appTypeArray[2]=="Application" || appTypeArray[2]=="Renewal"))
 {
     var hm = new Array();
@@ -29,27 +50,7 @@ if(!publicUser && (appTypeArray[1]=="Amendment" || appTypeArray[2]=="Application
         }
     }
 }
-try
-{
-    if(!publicUser)
-    {
-        setRecordAltID(capId);
-        if (parentCapId == "undefined" || parentCapId == null) {
-            parentCapId = aa.env.getValue("ParentCapID");
-            if(!parentCapId)
-                parentCapId = getParent();
-        }
-        if(parentCapId && (appMatch("Cannabis/*/Renewal/*") || appMatch("Cannabis/Amendment/*/*")))
-            updateShortNotes(getShortNotes(parentCapId));
-        else
-            updateShortNotes("PH3");
-    }
-}
-catch (err)
-{
-    aa.print("Error on changing sequence ASA: "+ err);
-    aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "Error on changing sequence ASA", err);
-}
+
 //Populate Geographic Information
 include("POPULATE_GEOGRAPHIC_INFORMATION");
 //Populate Geographic Information
--- EVENT/CTRCA;CANNABIS!~!~!~.js
+++ EVENT/CTRCA;CANNABIS!~!~!~.js
@@ -1,3 +1,21 @@
+try
+{
+    setRecordAltID(capId);
+    if (parentCapId == "undefined" || parentCapId == null) {
+        parentCapId = aa.env.getValue("ParentCapID");
+        if(!parentCapId)
+            parentCapId = getParent();
+    }
+    if(parentCapId && (appMatch("Cannabis/*/Renewal/*") || appMatch("Cannabis/Amendment/*/*")))
+        updateShortNotes(getShortNotes(parentCapId));
+    else
+        updateShortNotes("PH3");
+}
+catch (err)
+{
+    aa.print("Error on changing sequence ASA: "+ err);
+    aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "Error on changing sequence CTRCA", err);
+}
 if(appTypeArray[1]=="Amendment" || appTypeArray[2]=="Application" || appTypeArray[2]=="Renewal")
 {
     var hm = new Array();
@@ -28,24 +46,7 @@ if(appTypeArray[1]=="Amendment" || appTypeArray[2]=="Application" || appTypeArra
         }
     }
 }
-try
-{
-    setRecordAltID(capId);
-    if (parentCapId == "undefined" || parentCapId == null) {
-        parentCapId = aa.env.getValue("ParentCapID");
-        if(!parentCapId)
-            parentCapId = getParent();
-    }
-    if(parentCapId && (appMatch("Cannabis/*/Renewal/*") || appMatch("Cannabis/Amendment/*/*")))
-        updateShortNotes(getShortNotes(parentCapId));
-    else
-        updateShortNotes("PH3");
-}
-catch (err)
-{
-    aa.print("Error on changing sequence ASA: "+ err);
-    aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "Error on changing sequence CTRCA", err);
-}
+
 //Populate Geographic Information
 include("POPULATE_GEOGRAPHIC_INFORMATION");
 //Populate Geographic Information
