50e0f96b777bca357ea7d84465997706fbb94cbf
sshiralikar <sshiralikar@trustvip.com>
2024-03-20 10:43:09 -0400

    RSA

--- EVENT/RSA;~!~!~!~.js
+++ EVENT/RSA;~!~!~!~.js
@@ -1,56 +1,58 @@
-try {
+try
+{
     var publicUserModel = aa.env.getValue("PublicUserModel");
-    //var publicUserModel = aa.publicUser.getPublicUserByEmail("shash1@gmail.com").getOutput();
+    //var publicUserModel = aa.publicUser.getPublicUserByEmail("redapples@test.com").getOutput();
     var pUserSeqNumber = publicUserModel.getUserSeqNum();
-    var refCon = getRefConByPublicUserSeq(pUserSeqNumber);
+    var pUserEmail = publicUserModel.getEmail()+"";
+    assignCapIDsByEmail(pUserEmail,pUserSeqNumber);
+}
+catch(err)
+{
+    aa.sendMail("noreply-accela@northportfl.gov", "sshiralikar@trustvip.com", "", "Error on RSA", err);
+}
 
-    if (refCon) {
-        var peopleSequenceNumber = refCon.getContactSeqNumber()
-        var peopleModel = getOutput(aa.people.getPeople(peopleSequenceNumber), "");
+function assignCapIDsByEmail(email,pUserSeqNumber)
+{
+    var altIds = new Array();
+    var sql = "SELECT B1_PER_ID1, B1_PER_ID2, B1_PER_ID3 FROM B3CONTACT WHERE SERV_PROV_CODE='NORTHPORT' AND B1_EMAIL = '"+email+"'";
+    var res = doSQLSelect(sql);
+    for(var i in res)
+    {
+        var id1 = res[i].B1_PER_ID1;
+        var id2 = res[i].B1_PER_ID2;
+        var id3 = res[i].B1_PER_ID3;
+        var vCapId = aa.cap.getCapID(id1,id2,id3).getOutput();
+        aa.print("Assigning - "+ vCapId.getCustomID()+ " to "+ "PUBLICUSER"+pUserSeqNumber);
+        editCreatedByX("PUBLICUSER"+pUserSeqNumber, vCapId);
+    }
+}
 
-        var psm = aa.people.createPeopleModel().getOutput();
-        psm.setEmail(peopleModel.getEmail());
-        var cResult = aa.people.getCapIDsByRefContact(psm).getOutput();
 
-        if(cResult && cResult.length > 0)
-        {
-            for(var i in cResult)
-            {
-                var capId = aa.cap.getCapID(cResult[i].getID1(),cResult[i].getID2(),cResult[i].getID3()).getOutput();
-                editCreatedBy("PUBLICUSER"+pUserSeqNumber, capId);
-                aa.licenseScript.associateContactWithPublicUser(pUserSeqNumber, peopleSequenceNumber);
+function doSQLSelect(sql) {
+    var dq = aa.db.select(sql, []);
+    if (dq.getSuccess()) {
+        var dso = dq.getOutput();
+        if (dso) {
+            var a = [];
+            var ds = dso.toArray();
+            for (var x in ds) {
+                var r = {};
+                var row = ds[x];
+                var ks = ds[x].keySet().toArray();
+                for (var c in ks) {
+                    r[ks[c]] = String(row.get(ks[c]));
+                    aa.print(ks[c] + ": " + (row.get(ks[c])));
+                }
+                a.push(r);
             }
         }
-    }
-} catch (err) {
-    aa.debug("ContactRelatedToPublicUser Error", err)
-}
-
-function getRefConByPublicUserSeq(pSeqNum) {
-
-    var publicUserSeq = pSeqNum; //Public user sequence number
-    var userSeqList = aa.util.newArrayList();
-    userSeqList.add(aa.util.parseLong(publicUserSeq));
-    var contactPeopleBiz = aa.proxyInvoker.newInstance("com.accela.pa.people.ContractorPeopleBusiness").getOutput();
-    var contactors = contactPeopleBiz.getContractorPeopleListByUserSeqNBR("MENDOCINO", userSeqList);
-    if (contactors) {
-        if (contactors.size() > 0) {
-            if (contactors.get(0)) {
-                return contactors.get(0);
-            }
-        }
-    }
-    return false;
-}
-function getOutput(result, object) {
-    if (result.getSuccess()) {
-        return result.getOutput();
-    }
-    else {
-        return null;
+        //aa.print(JSON.stringify(a) + "<br>");
+        return a;
+    } else {
+        aa.print("error " + dq.getErrorMessage());
     }
 }
-function editCreatedBy(nCreatedBy) {
+function editCreatedByX(nCreatedBy) {
     // 4/30/08 - DQ - Corrected Error where option parameter was ignored
     var itemCap = capId;
     if (arguments.length == 2) itemCap = arguments[1]; // use cap ID specified in args
@@ -68,7 +70,13 @@ function editCreatedBy(nCreatedBy) {
     setCreatedByResult = aa.cap.editCapByPK(capEModel);
 
     if (!setCreatedByResult.getSuccess())
-    { aa.print("**WARNING: error setting cap created by : " + setCreatedByResult.getErrorMessage()) ; return false }
+    {
+        aa.print("**WARNING: error setting cap created by : " + setCreatedByResult.getErrorMessage()) ; return false
+    }
+    else
+    {
+        aa.print("Edit Successful");
+    }
 
     return true;
 }
