fbaea4919e216bc982476a24c83a48bd3f461f91
sumpatel640 <160267547+sumpatel640@users.noreply.github.com>
2024-03-28 17:06:10 -0400

    Revert "Update ASA;CANNABIS!~!~!~.js"
    This reverts commit 094aa56124c7d34218d4a0b5e9906e3dc56b4308.

--- EVENT/ASA;CANNABIS!~!~!~.js
+++ EVENT/ASA;CANNABIS!~!~!~.js
@@ -19,43 +19,11 @@ catch (err)
     aa.print("Error on changing sequence ASA: "+ err);
     aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "Error on changing sequence ASA", err);
 }
-if(!publicUser) {
-    if(appTypeArray[1]=="Amendment" || appTypeArray[2]=="Application") {
-        var hm = new Array();
-        var conName = "";
-        var contactResult = aa.people.getCapContactByCapID(capId);
-        if (contactResult.getSuccess()) {
-            var capContacts = contactResult.getOutput();
-            for (var i in capContacts) {
-                if(matches(capContacts[i].getPeople().getContactType(),"Applicant","Authorized Agent"))
-                {
-                    conName = getContactName(capContacts[i]);
-                    var params = aa.util.newHashtable();
-                    addParameter(params, "$$altID$$", capId.getCustomID()+"");
-                    addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias()+"");
-                    addParameter(params, "$$capName$$", capName);
-                    addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptName"));
-                    addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
-                    addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
-                    addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
-                    addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
-                    addParameter(params, "$$FullNameBusName$$", conName);
-                    addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
-                    addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
-                    if(hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
-                        sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "GLOBAL_APPLICATION_SUBMITTED", params, null, capId);
-                        hm[capContacts[i].getPeople().getEmail() + ""] = 1;
-                    }
-                }
-            }
-        }
-    }
-    if(appTypeArray[2]=="Renewal") {
-// Updated email for CAMEND-478 to use Async script so Renewal in back office (clone) pulls new Alt Id
-        var envParameters = aa.util.newHashMap();
-        envParameters.put("capIdStr", capId.getCustomID()+"");
-        aa.runAsyncScript("ASYNC_SEND_SUBMISSION_EMAIL", envParameters);
-    }
+if(!publicUser && (appTypeArray[1]=="Amendment" || appTypeArray[2]=="Application" || appTypeArray[2]=="Renewal"))
+{
+    var envParameters = aa.util.newHashMap();
+    envParameters.put("capIdStr", capId.getCustomID()+"");
+    aa.runAsyncScript("ASYNC_SEND_SUBMISSION_EMAIL", envParameters);
 }
 
 //Populate Geographic Information
