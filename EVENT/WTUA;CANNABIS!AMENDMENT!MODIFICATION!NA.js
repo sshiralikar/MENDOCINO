c55e70c521cafb541416fb807c145d635aa377e8
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2024-10-10 16:30:21 -0400

    CAMEND-512, CAMEND-507

--- EVENT/WTUA;CANNABIS!AMENDMENT!MODIFICATION!NA.js
+++ EVENT/WTUA;CANNABIS!AMENDMENT!MODIFICATION!NA.js
@@ -301,82 +301,82 @@ if(wfTask == "Draft Decision" && wfStatus == "Denied")
 //     }
 // }
 
-// CAMEND-512 led to this to be commented out due to deleting workflow task supervisor review.
-// if(wfStatus == "Deficiency")
-// {
-//     editPriority("Low");
-//     //if(wfTask == "Supervisor Review"){
-//     var c = new Date();
-//     var newDate = c.getMonth()+1+"/"+c.getDate()+"/"+c.getFullYear();
-//     editTaskDueDate(wfTask, dateAdd(newDate, 15));
-//     //}
 
-// }
+if(wfStatus == "Deficiency")
+{
+    editPriority("Low");
+    //if(wfTask == "Supervisor Review"){
+    var c = new Date();
+    var newDate = c.getMonth()+1+"/"+c.getDate()+"/"+c.getFullYear();
+    editTaskDueDate(wfTask, dateAdd(newDate, 15));
+    //}
 
-// CAMEND-512 led to this to be commented out due to deleting workflow task supervisor review.
-// //CAMEND-392
-// if(wfTask == "Supervisor Review" && wfStatus == "Deficiency")
-// {
-//     //var date = getCapFileDate(capId);
-//     //if(isDateInRangeToOct(date) || isDateInRangeToFeb(date) || isDateInRangeCurr(date)) {
-//     var rFiles = [];
-//     var hm = new Array();
-//     var conName = "";
-//     var rParams = aa.util.newHashMap();
-//     rParams.put("RecordID", capId.getCustomID()+"");
-//     logDebug("Report parameter RecordID set to: "+ capId.getCustomID()+"");
-//     var report = aa.reportManager.getReportInfoModelByName("Cannabis Deficiency Denial Pending Letter");
-//     report = report.getOutput();
-//     report.setModule("Cannabis");
-//     report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
-//     report.setReportParameters(rParams);
-//     report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
+}
 
 
-//     var permit = aa.reportManager.hasPermission("Cannabis Deficiency Denial Pending Letter",currentUserID);
+//CAMEND-392, CAMEND-512
+if(wfTask == "Draft Decision" && wfStatus == "Deficiency")
+{
+    //var date = getCapFileDate(capId);
+    //if(isDateInRangeToOct(date) || isDateInRangeToFeb(date) || isDateInRangeCurr(date)) {
+    var rFiles = [];
+    var hm = new Array();
+    var conName = "";
+    var rParams = aa.util.newHashMap();
+    rParams.put("RecordID", capId.getCustomID()+"");
+    logDebug("Report parameter RecordID set to: "+ capId.getCustomID()+"");
+    var report = aa.reportManager.getReportInfoModelByName("Cannabis Deficiency Denial Pending Letter");
+    report = report.getOutput();
+    report.setModule("Cannabis");
+    report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
+    report.setReportParameters(rParams);
+    report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
 
-//     if (permit.getOutput().booleanValue()) {
-//         logDebug("User has Permission to run the report....");
-//         var reportResult = aa.reportManager.getReportResult(report);
-//         if(reportResult) {
-//             reportOutput = reportResult.getOutput();
-//             var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
-//             logDebug("Report Run Successfull:"+ reportFile.getSuccess());
-//             reportFile=reportFile.getOutput();
-//             rFiles.push(reportFile);
-//         }
-//     }
-//     var contactResult = aa.people.getCapContactByCapID(capId);
-//     if (contactResult.getSuccess()) {
-//         var capContacts = contactResult.getOutput();
-//         for (var i in capContacts) {
-//             if(matches(capContacts[i].getPeople().getContactType(),"Applicant","Authorized Agent","Property Owner"))
-//             {
-//                 conName = getContactName(capContacts[i]);
-//                 var params = aa.util.newHashtable();
-//                 addParameter(params, "$$altID$$", capId.getCustomID()+"");
-//                 addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias()+"");
-//                 addParameter(params, "$$capName$$", capName);
-//                 addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptName"));
-//                 addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
-//                 addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
-//                 addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
-//                 addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
-//                 addParameter(params, "$$contactname$$", conName);
-//                 addParameter(params, "$$date$$", sysDateMMDDYYYY);
-//                 addParameter(params, "$$contactEmail$$", capContacts[i].getPeople().getEmail() + "");
-//                 addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
-//                 addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
-//                 if(hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
 
-//                     sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "CAN_DEFICIENCY", params, rFiles, capId);
-//                     hm[capContacts[i].getPeople().getEmail() + ""] = 1;
-//                 }
-//             }
-//         }
-//         //}
-//     }
-// }
+    var permit = aa.reportManager.hasPermission("Cannabis Deficiency Denial Pending Letter",currentUserID);
+
+    if (permit.getOutput().booleanValue()) {
+        logDebug("User has Permission to run the report....");
+        var reportResult = aa.reportManager.getReportResult(report);
+        if(reportResult) {
+            reportOutput = reportResult.getOutput();
+            var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
+            logDebug("Report Run Successfull:"+ reportFile.getSuccess());
+            reportFile=reportFile.getOutput();
+            rFiles.push(reportFile);
+        }
+    }
+    var contactResult = aa.people.getCapContactByCapID(capId);
+    if (contactResult.getSuccess()) {
+        var capContacts = contactResult.getOutput();
+        for (var i in capContacts) {
+            if(matches(capContacts[i].getPeople().getContactType(),"Applicant","Authorized Agent","Property Owner"))
+            {
+                conName = getContactName(capContacts[i]);
+                var params = aa.util.newHashtable();
+                addParameter(params, "$$altID$$", capId.getCustomID()+"");
+                addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias()+"");
+                addParameter(params, "$$capName$$", capName);
+                addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptName"));
+                addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
+                addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
+                addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
+                addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
+                addParameter(params, "$$contactname$$", conName);
+                addParameter(params, "$$date$$", sysDateMMDDYYYY);
+                addParameter(params, "$$contactEmail$$", capContacts[i].getPeople().getEmail() + "");
+                addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
+                addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
+                if(hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
+
+                    sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "CAN_DEFICIENCY", params, rFiles, capId);
+                    hm[capContacts[i].getPeople().getEmail() + ""] = 1;
+                }
+            }
+        }
+        //}
+    }
+}
 function copyASITablesWithRemove(pFromCapId, pToCapId) {
     // Function dependencies on addASITable()
     // par3 is optional 0 based string array of table to ignore
