0ddd54c5af6d4bd1c46bc8545fe543b2f2cceff5
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2024-10-11 14:39:53 -0400

    CAMEND-574

--- EVENT/CTRCA;CANNABIS!~!~!~.js
+++ EVENT/CTRCA;CANNABIS!~!~!~.js
@@ -1,29 +1,26 @@
 var newAltId = "";
-try
-{
+try {
     newAltId = setRecordAltID(capId);
     if (parentCapId == "undefined" || parentCapId == null) {
         parentCapId = aa.env.getValue("ParentCapID");
-        if(!parentCapId)
+        if (!parentCapId)
             parentCapId = getParent();
     }
-    if(parentCapId && (appMatch("Cannabis/*/Renewal/*") || appMatch("Cannabis/Amendment/*/*")))
+    if (parentCapId && (appMatch("Cannabis/*/Renewal/*") || appMatch("Cannabis/Amendment/*/*")))
         updateShortNotes(getShortNotes(parentCapId));
     else
         updateShortNotes("PH3");
 }
-catch (err)
-{
-    aa.print("Error on changing sequence CTRCA: "+ err);
+catch (err) {
+    aa.print("Error on changing sequence CTRCA: " + err);
     aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "Error on changing sequence CTRCA", err);
 }
-if(appTypeArray[1]=="Amendment" || appTypeArray[2]=="Application" || appTypeArray[2]=="Renewal")
-{
+if (appTypeArray[1] == "Amendment" || appTypeArray[2] == "Application" || appTypeArray[2] == "Renewal") {
     var envParameters = aa.util.newHashMap();
-    if(newAltId!="")
-        envParameters.put("capIdStr", newAltId+"");
+    if (newAltId != "")
+        envParameters.put("capIdStr", newAltId + "");
     else
-        envParameters.put("capIdStr", capId.getCustomID()+"");
+        envParameters.put("capIdStr", capId.getCustomID() + "");
     aa.runAsyncScript("ASYNC_SEND_SUBMISSION_EMAIL", envParameters);
 }
 
@@ -31,18 +28,18 @@ if(appTypeArray[1]=="Amendment" || appTypeArray[2]=="Application" || appTypeArra
 include("POPULATE_GEOGRAPHIC_INFORMATION");
 //Populate Geographic Information
 
-try
-{
+try {
     createRefLicProfFromLicProf();
 }
-catch(err)
-{
+catch (err) {
     logDebug("LP Update not necessary");
 }
 
-//CAMEND-574
-if (appMatch("Cannabis/Amendment/Assignment/NA")) {
-    if (AInfo["Vegetation Removal Purpose"] == "Yes") {
-        addStdConditionX("Vegetation", "Tree Removal Identified");
+if (publicUser) {
+    //CAMEND-574
+    if (appMatch("Cannabis/Amendment/Assignment/NA")) {
+        if (AInfo["Vegetation Removal Purpose"] == "Yes") {
+            addStdConditionX("Vegetation", "Tree Removal Identified");
+        }
     }
 }
