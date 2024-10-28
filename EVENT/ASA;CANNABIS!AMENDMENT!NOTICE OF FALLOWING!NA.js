d3ef0f5715f805afbec9161dcc67548158ebc866
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2024-10-28 16:15:43 -0400

    CAMEND-566

--- EVENT/ASA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!NA.js
+++ EVENT/ASA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!NA.js
@@ -23,10 +23,10 @@ if (capDetailObjResult.getSuccess()) {
             for (var i in capContacts) {
                 conName = getContactName(capContacts[i]);
                 var params = aa.util.newHashtable();
-                addParameter(params, "$$altID$$", capId.getCustomID() + "");
-                addParameter(params, "$$year$$", new Date().getFullYear());
+                addParameter(params, "$$altID$$", pCapId.getCustomID() + "");
+                addParameter(params, "$$year$$", String(aa.date.getCurrentDate().getYear()));
                 addParameter(params, "$$date$$", sysDateMMDDYYYY);
-                addParameter(params, "$$parentAltId$$", pCapId.getCustomID() + "");
+                addParameter(params, "$$parentAltId$$", pCapId.getCustomID()+"");
                 addParameter(params, "$$contactname$$", conName);
                 addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptName"));
                 addParameter(params, "$$phoneHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "phoneHours"));
@@ -34,17 +34,13 @@ if (capDetailObjResult.getSuccess()) {
                 addParameter(params, "$$officeHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "officeHours"));
                 addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptHours"));
                 addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptEmail"));
-                addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptAddress"));
+                addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptAddress"));
                 addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptFormalName"));
                 addParameter(params, "$$FullNameBusName$$", conName);
                 addParameter(params, "$$capAlias$$", aa.cap.getCap(pCapId).getOutput().getCapType().getAlias() + "");
                 addParameter(params, "$$parentCapId$$", pCapId.getCustomID());
                 addParameter(params, "$$Amendment$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias() + "");
                 addParameter(params, "$$Location$$", getAddressInALine());
-                if (wfComment != "" && wfComment != null)
-                    addParameter(params, "$$wfComment$$", "Comments: " + wfComment);
-                else
-                    addParameter(params, "$$wfComment$$", "");
                 sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "CAN_NOF_SUBMITTED", params, null, capId);
             }
         }
