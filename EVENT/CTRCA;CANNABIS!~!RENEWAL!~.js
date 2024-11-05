39f1210890f4e6a7b586c398cfcc712eec132621
sshiralikar <sshiralikar@trustvip.com>
2024-11-04 23:55:42 -0500

    Bug

--- EVENT/CTRCA;CANNABIS!~!RENEWAL!~.js
+++ EVENT/CTRCA;CANNABIS!~!RENEWAL!~.js
@@ -1,8 +1,7 @@
-parentCapId = getParentCapID4Renewal();
 updateAppStatus("Pending","Updated through script");
 if(parentCapId)
     updateAppStatus("Renewal Under Review","Updated through script",parentCapId);
-if(parentCapId && AInfo["Structure Change"] == "Yes")
+if(AInfo["Structure Change"] == "Yes")
 {
     taskCloseAllExcept("Modification Required","Closing via script");
     updateAppStatus("Modification Required","Updating via Script");
