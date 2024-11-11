9bc3153a3849e2fe234efb0a1addb0251a416175
sshiralikar <sshiralikar@trustvip.com>
2024-11-11 10:46:43 -0500

    CAMEND-612

--- EVENT/CTRCA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!REVOCATION.js
+++ EVENT/CTRCA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!REVOCATION.js
@@ -5,5 +5,5 @@ if(parentCapId!=null && parentCapId!="")
     licCapId = parentCapId;
 updateAppStatus("Pending Fallowing Revocation","",licCapId);
 updateTask("Permit Status","Pending Fallowing Revocation","","",licCapId);
-
+logDebug("sysDateMMDDYYYY: "+ sysDateMMDDYYYY);
 editAppSpecific("ROF Submitted Date", sysDateMMDDYYYY, licCapId);
