1c19a3fc4869b7438abd56043402d5e1763ab151
sshiralikar <sshiralikar@trustvip.com>
2024-11-11 10:25:43 -0500

    CAMEND-612

--- EVENT/ASA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!REVOCATION.js
+++ EVENT/ASA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!REVOCATION.js
@@ -2,6 +2,8 @@
 if(!publicUser)
 {
     var licCapId = getParent();
+    if(parentCapId!=null && parentCapId!="")
+        licCapId = parentCapId;
     updateAppStatus("Pending Fallowing Revocation","",licCapId);
     updateTask("Permit Status","Pending Fallowing Revocation","","",licCapId);
 }
--- EVENT/CTRCA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!REVOCATION.js
+++ EVENT/CTRCA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!REVOCATION.js
@@ -1,5 +1,7 @@
 //CAMEND-612
 
 var licCapId = getParent();
+if(parentCapId!=null && parentCapId!="")
+    licCapId = parentCapId;
 updateAppStatus("Pending Fallowing Revocation","",licCapId);
 updateTask("Permit Status","Pending Fallowing Revocation","","",licCapId);
