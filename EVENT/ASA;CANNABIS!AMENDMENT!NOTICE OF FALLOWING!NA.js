a48c6c6790c8816b8ec87b6d3ce9376f989c03f9
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2024-11-13 11:55:44 -0500

    CAMEND-633

--- EVENT/ASA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!NA.js
+++ EVENT/ASA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!NA.js
@@ -4,4 +4,12 @@ if (!publicUser) {
     if (parentCapId != null && parentCapId != "")
         licCapId = parentCapId;
     editAppSpecific("NOF Submitted Date", sysDateMMDDYYYY, licCapId);
+
+    // CAMEND-633
+    var totalSFinUse = 0;
+    if (AInfo["Partial SF/Partial Nursery SF"] != null) {
+        totalSFinUse = AInfo["Total SF/Total Nursery SF"] - AInfo["Partial SF/Partial Nursery SF"];
+    }
+    logDebug("Total SF In Use: " + totalSFinUse);
+    editAppSpecific("Total SF In Use", totalSFinUse, licCapId);
 }
--- EVENT/CTRCA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!NA.js
+++ EVENT/CTRCA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!NA.js
@@ -1,6 +1,14 @@
 //CAMEND-620
 var licCapId = getParent();
-if(parentCapId!=null && parentCapId!="")
+if (parentCapId != null && parentCapId != "")
     licCapId = parentCapId;
-logDebug("sysDateMMDDYYYY: "+ sysDateMMDDYYYY);
+logDebug("sysDateMMDDYYYY: " + sysDateMMDDYYYY);
 editAppSpecific("NOF Submitted Date", sysDateMMDDYYYY, licCapId);
+
+// CAMEND-633
+var totalSFinUse = 0;
+if (AInfo["Partial SF/Partial Nursery SF"] != null) {
+    totalSFinUse = AInfo["Total SF/Total Nursery SF"] - AInfo["Partial SF/Partial Nursery SF"];
+}
+logDebug("Total SF In Use: " + totalSFinUse);
+editAppSpecific("Total SF In Use", totalSFinUse, licCapId);
