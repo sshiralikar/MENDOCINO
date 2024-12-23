c0daf0f04616be2783c819fe66aea9946e9c79eb
sshiralikar <sshiralikar@trustvip.com>
2024-12-23 15:02:43 -0500

    CAMEND-677

--- EVENT/WTUA;CANNABIS!AMENDMENT!NOTICE OF NON-CULTIVATION!NA.js
+++ EVENT/WTUA;CANNABIS!AMENDMENT!NOTICE OF NON-CULTIVATION!NA.js
@@ -1,37 +1,22 @@
-try {
-    if(!publicUser)
-    {
-        updateAppStatus("Amendment Review","Approved");
-        updateTask("Amendment Review","Approved","","");
-        aa.workflow.adjustTask(capId, "Amendment Review", "Y", "N", null, null);
-
-        if(parentCapId)
-        {
-            editAppSpecific("NONC Submitted Date", sysDateMMDDYYYY, parentCapId);
-            var today = new Date();
-            today.setFullYear(today.getFullYear() + 1);
-            var newDate = today.getMonth()+1+"/"+today.getDate()+"/"+today.getFullYear();
-            editAppSpecific("NONC Expiration Date", newDate, parentCapId);
-            setLicExpirationDate(parentCapId,"",newDate);
-            editAppSpecific("New Expiration Date", newDate, parentCapId);
-
-            var today = new Date();
-            today.setFullYear(today.getFullYear() + 5);
-            var newDate = today.getMonth()+1+"/"+today.getDate()+"/"+today.getFullYear();
-            editAppSpecific("NONC Requested Expiration Date", newDate, parentCapId);
-            updateAppStatus("Notice of Non Cultivation","updated via script", parentCapId);
-
+// CAMEND-563
+if (wfStatus == "Approved") {
+    var pCapId = getParent();
+    var capDetailObjResult = aa.cap.getCapDetail(pCapId); // Detail
+    if (capDetailObjResult.getSuccess()) {
+        capDetail = capDetailObjResult.getOutput();
+        var balanceDue = capDetail.getBalance();
+        if (balanceDue <= 0) {
             var conName = "";
-            var contactResult = aa.people.getCapContactByCapID(parentCapId);
+            var contactResult = aa.people.getCapContactByCapID(pCapId);
             if (contactResult.getSuccess()) {
                 var capContacts = contactResult.getOutput();
                 for (var i in capContacts) {
                     conName = getContactName(capContacts[i]);
                     var params = aa.util.newHashtable();
-                    addParameter(params, "$$altID$$", parentCapId.getCustomID() + "");
+                    addParameter(params, "$$altID$$", pCapId.getCustomID() + "");
                     addParameter(params, "$$year$$", String(aa.date.getCurrentDate().getYear()));
                     addParameter(params, "$$date$$", sysDateMMDDYYYY);
-                    addParameter(params, "$$parentAltId$$", parentCapId.getCustomID()+"");
+                    addParameter(params, "$$parentAltId$$", pCapId.getCustomID()+"");
                     addParameter(params, "$$contactname$$", conName);
                     addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptName"));
                     addParameter(params, "$$phoneHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "phoneHours"));
@@ -42,8 +27,8 @@ try {
                     addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptAddress"));
                     addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptFormalName"));
                     addParameter(params, "$$FullNameBusName$$", conName);
-                    addParameter(params, "$$capAlias$$", aa.cap.getCap(parentCapId).getOutput().getCapType().getAlias() + "");
-                    addParameter(params, "$$parentCapId$$", parentCapId.getCustomID());
+                    addParameter(params, "$$capAlias$$", aa.cap.getCap(pCapId).getOutput().getCapType().getAlias() + "");
+                    addParameter(params, "$$parentCapId$$", pCapId.getCustomID());
                     addParameter(params, "$$Amendment$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias() + "");
                     addParameter(params, "$$Location$$", getAddressInALine());
                     sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", String(lookup("TTC EMAIL", "EMAIL"))+"", "CAN_NONC_APPROVED", params, null, capId);
@@ -51,6 +36,4 @@ try {
             }
         }
     }
-} catch (err) {
-    logDebug("A Javascript error has occurred within the following file: ASA:Cannabis/Amendment/Notice of Non-Cultivation/NA" + err.message);
 }
