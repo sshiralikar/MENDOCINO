626b68e6a5d570a31ff2985d0948ab8fc1661915
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2024-12-26 15:46:38 -0500

    CAMEND-586

--- EVENT/CTRCA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!AFFIDAVIT.js
+++ EVENT/CTRCA;CANNABIS!AMENDMENT!NOTICE OF FALLOWING!AFFIDAVIT.js
@@ -1,6 +1,21 @@
-//CAMEND-620
+//CAMEND-620 & CAMEND-586
+updateAppStatus("Amendment Review", "Approved");
+updateTask("Amendment Review", "Approved", "", "");
+aa.workflow.adjustTask(capId, "Amendment Review", "Y", "N", null, null);
 var licCapId = getParent();
-if(parentCapId!=null && parentCapId!="")
+if (parentCapId != null && parentCapId != "")
     licCapId = parentCapId;
-logDebug("sysDateMMDDYYYY: "+ sysDateMMDDYYYY);
+logDebug("sysDateMMDDYYYY: " + sysDateMMDDYYYY);
 editAppSpecific("FA Submitted Date", sysDateMMDDYYYY, licCapId);
+
+var capCondResult = aa.capCondition.getCapConditions(parentCapId);
+if (capCondResult.getSuccess()) {
+    var coArray = capCondResult.getOutput();
+    for (co in coArray) {
+        if (coArray[co].getConditionDescription() == "Fallowing Affidavit Required" && coArray[co].getConditionStatus() == "Applied") {
+            coArray[co].setConditionStatus("Met");
+            aa.capCondition.editCapCondition(coArray[co]);
+            logDebug("Fallowing Affidavit Required has been Met");
+        }
+    }
+}
