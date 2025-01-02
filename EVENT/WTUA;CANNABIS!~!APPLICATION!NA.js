b974e87965e484a9cf91be4fe757d6405e301d5a
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-01-02 14:59:30 -0500

    CAMEND-692

--- EVENT/WTUA;CANNABIS!~!APPLICATION!NA.js
+++ EVENT/WTUA;CANNABIS!~!APPLICATION!NA.js
@@ -477,6 +477,7 @@ if(wfTask == "Supervisor Review" && wfStatus == "Deficiency")
 //     }
 // }
 if (wfStatus == "Approved") {
+    var hm = new Array();
     var parent = getParent();
     var parentCap = aa.cap.getCap(parentCapId).getOutput();
     parentAppTypeResult = parentCap.getCapType();
