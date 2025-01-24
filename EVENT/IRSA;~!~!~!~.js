cb23b8c8682d7084f9d47dd3f42020c57a470254
sshiralikar <sshiralikar@trustvip.com>
2025-01-23 21:42:44 -0500

    CAMEND-635

--- EVENT/IRSA;~!~!~!~.js
+++ EVENT/IRSA;~!~!~!~.js
@@ -33,6 +33,26 @@ if (contactResult.getSuccess()) {
                     rFiles.push(reportFile);
                 }
             }
+            var rParams = aa.util.newHashMap();
+            rParams.put("inspectionid", inspId);
+            rParams.put("MobileInd", "1");
+            var report = aa.reportManager.getReportInfoModelByName("Cannabis Inspection Mobile Report");
+            report = report.getOutput();
+            report.setModule("Cannabis");
+            report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
+            report.setReportParameters(rParams);
+            report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
+            var permit = aa.reportManager.hasPermission("Cannabis Inspection Mobile Report",reportUser);
+
+            if (permit.getOutput().booleanValue()) {
+                var reportResult = aa.reportManager.getReportResult(report);
+                if(reportResult) {
+                    reportOutput = reportResult.getOutput();
+                    var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
+                    reportFile=reportFile.getOutput();
+                    rFiles.push(reportFile);
+                }
+            }
             VRFiles = rFiles;
             var params = aa.util.newHashtable();
             addParameter(params, "$$InspectorOfRecord1$$", inspectorName);
