f4c95e0923c064cb941a4ebe8b9342fe3da74a4e
sshiralikar <sshiralikar@trustvip.com>
2025-01-28 21:14:09 -0500

    CAMEND-819

---
+++ BATCH/BATCH_PURGE_TMP_RECORDS.js
@@ -0,0 +1,19 @@
+var batchJobName = "BATCH_PURGE_TMP_RECORDS";
+var batchJobDesc = "BATCH_PURGE_TMP_RECORDS";
+var batchJobResult = "BATCH_PURGE_TMP_RECORDS";
+var sysDate = aa.date.getCurrentDate();
+var batchJobID = aa.batchJob.getJobID().getOutput();
+var removeResult = aa.cap.removeExpiredIncompleteCAP();
+if(removeResult.getSuccess())
+{
+    aa.print("passed");
+    aa.env.setValue("ScriptReturnCode","0");
+    aa.env.setValue("ScriptReturnMessage","Remove expired incomplete CAPS successful");
+    aa.eventLog.createEventLog("Cleared Incomplete CAPs successfully", "Batch Process", batchJobName, sysDate, sysDate,batchJobDesc, batchJobResult, batchJobID);
+}
+else
+{
+    aa.print("failed");
+    aa.env.setValue("ScriptReturnCode","1");
+    aa.env.setValue("ScriptReturnMessage","Remove expired incomplete CAPS failed");
+}
