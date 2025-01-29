b06ab338c6cd8258eaeb4d4b4f4f2286b4a5f0a1
sshiralikar <sshiralikar@trustvip.com>
2025-01-29 10:04:32 -0500

    CAMEND-819

--- EVENT/IRSA;~!~!~!~.js
+++ EVENT/IRSA;~!~!~!~.js
@@ -1,45 +1,65 @@
+var isMobile = false;
+var capDocResult = aa.document.getDocumentListByEntity(capId+"-"+inspId,null);
+if(capDocResult.getSuccess()) {
+    if(capDocResult.getOutput().size() > 0) {
+        /*for(docInx = 0; docInx < capDocResult.getOutput().size(); docInx++)
+        {
+            var documentObject = capDocResult.getOutput().get(docInx);
+            currDocCat = "" + documentObject.getDocCategory();
+            var fileName = documentObject.getFileName();
+            aa.print("orgfileName: "+ fileName+" - "+ documentObject.getEntityType());
+        }*/
+        isMobile = true;
+    }
+}
 var hm = new Array();
 var reportUser = "ADMIN";
 var rFiles = [];
 var VRFiles = null;
-var rParams = aa.util.newHashMap();
-rParams.put("inspectionid", inspId+"");
-rParams.put("MobileInd", "0");
-var report = aa.reportManager.getReportInfoModelByName("Cannabis Inspection Report");
-report = report.getOutput();
-report.setModule("Cannabis");
-report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
-report.setReportParameters(rParams);
-report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
-var permit = aa.reportManager.hasPermission("Cannabis Inspection Report",reportUser);
+if(!isMobile)
+{
+    var rParams = aa.util.newHashMap();
+    rParams.put("inspectionid", inspId+"");
+    rParams.put("MobileInd", "0");
+    var report = aa.reportManager.getReportInfoModelByName("Cannabis Inspection Report");
+    report = report.getOutput();
+    report.setModule("Cannabis");
+    report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
+    report.setReportParameters(rParams);
+    report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
+    var permit = aa.reportManager.hasPermission("Cannabis Inspection Report",reportUser);
 
-if (permit.getOutput().booleanValue()) {
-    var reportResult = aa.reportManager.getReportResult(report);
-    if(reportResult) {
-        reportOutput = reportResult.getOutput();
-        var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
-        reportFile=reportFile.getOutput();
-        rFiles.push(reportFile);
+    if (permit.getOutput().booleanValue()) {
+        var reportResult = aa.reportManager.getReportResult(report);
+        if(reportResult) {
+            reportOutput = reportResult.getOutput();
+            var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
+            reportFile=reportFile.getOutput();
+            rFiles.push(reportFile);
+        }
     }
 }
-var rParams = aa.util.newHashMap();
-rParams.put("inspectionid", inspId+"");
-rParams.put("MobileInd", "1");
-var report = aa.reportManager.getReportInfoModelByName("Cannabis Inspection Mobile Report");
-report = report.getOutput();
-report.setModule("Cannabis");
-report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
-report.setReportParameters(rParams);
-report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
-var permit = aa.reportManager.hasPermission("Cannabis Inspection Mobile Report",reportUser);
+else
+{
+    var rParams = aa.util.newHashMap();
+    rParams.put("inspectionid", inspId+"");
+    rParams.put("MobileInd", "1");
+    var report = aa.reportManager.getReportInfoModelByName("Cannabis Inspection Mobile Report");
+    report = report.getOutput();
+    report.setModule("Cannabis");
+    report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
+    report.setReportParameters(rParams);
+    report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
+    var permit = aa.reportManager.hasPermission("Cannabis Inspection Mobile Report",reportUser);
 
-if (permit.getOutput().booleanValue()) {
-    var reportResult = aa.reportManager.getReportResult(report);
-    if(reportResult) {
-        reportOutput = reportResult.getOutput();
-        var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
-        reportFile=reportFile.getOutput();
-        rFiles.push(reportFile);
+    if (permit.getOutput().booleanValue()) {
+        var reportResult = aa.reportManager.getReportResult(report);
+        if(reportResult) {
+            reportOutput = reportResult.getOutput();
+            var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
+            reportFile=reportFile.getOutput();
+            rFiles.push(reportFile);
+        }
     }
 }
 VRFiles = rFiles;
