1c2b7676011fc3e8d1e36b58387644cb0d9f2198
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-01-31 09:34:56 -0500

    CAMEND-780

--- EVENT/IRSA;~!~!~!~.js
+++ EVENT/IRSA;~!~!~!~.js
@@ -126,3 +126,18 @@ if (contactResult.getSuccess()) {
         }
     }
 }
+
+// CAMEND-780
+if (appMatch("Cannabis/*/Application/NA")) {
+    if ((inspType == "Cultivation - Site Inspection" ||
+        inspType == "Cultivation - Special Inspection" ||
+        inspType == "Nursery - Site Inspection" ||
+        inspType == "Nursery - Special Inspection") && (inspResult == "Pass" || inspResult == "Fail")) {
+
+        addStdConditionX("Cannabis Required Document", "CCBL Affidavit");
+        // updateAppStatus("Closed","Closed automatically via Script", capId);
+        updateTask("Site Inspection","Pass","","");
+        aa.workflow.adjustTask(capId, "Site Inspection", "N", "Y", null, null);
+        aa.workflow.adjustTask(capId, "Draft Decision", "Y", "N", null, null);
+    }
+}
