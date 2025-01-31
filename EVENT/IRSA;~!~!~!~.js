acf6cabc9ef7b3b0360cf45b91e98194a213f9cb
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-01-31 10:04:48 -0500

    CAMEND-780

--- EVENT/IRSA;~!~!~!~.js
+++ EVENT/IRSA;~!~!~!~.js
@@ -1,7 +1,7 @@
 var isMobile = false;
-var capDocResult = aa.document.getDocumentListByEntity(capId+"-"+inspId,null);
-if(capDocResult.getSuccess()) {
-    if(capDocResult.getOutput().size() > 0) {
+var capDocResult = aa.document.getDocumentListByEntity(capId + "-" + inspId, null);
+if (capDocResult.getSuccess()) {
+    if (capDocResult.getOutput().size() > 0) {
         /*for(docInx = 0; docInx < capDocResult.getOutput().size(); docInx++)
         {
             var documentObject = capDocResult.getOutput().get(docInx);
@@ -16,10 +16,9 @@ var hm = new Array();
 var reportUser = "ADMIN";
 var rFiles = [];
 var VRFiles = null;
-if(!isMobile)
-{
+if (!isMobile) {
     var rParams = aa.util.newHashMap();
-    rParams.put("inspectionid", inspId+"");
+    rParams.put("inspectionid", inspId + "");
     rParams.put("MobileInd", "0");
     var report = aa.reportManager.getReportInfoModelByName("Cannabis Inspection Report");
     report = report.getOutput();
@@ -27,22 +26,21 @@ if(!isMobile)
     report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
     report.setReportParameters(rParams);
     report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
-    var permit = aa.reportManager.hasPermission("Cannabis Inspection Report",reportUser);
+    var permit = aa.reportManager.hasPermission("Cannabis Inspection Report", reportUser);
 
     if (permit.getOutput().booleanValue()) {
         var reportResult = aa.reportManager.getReportResult(report);
-        if(reportResult) {
+        if (reportResult) {
             reportOutput = reportResult.getOutput();
-            var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
-            reportFile=reportFile.getOutput();
+            var reportFile = aa.reportManager.storeReportToDisk(reportOutput);
+            reportFile = reportFile.getOutput();
             rFiles.push(reportFile);
         }
     }
 }
-else
-{
+else {
     var rParams = aa.util.newHashMap();
-    rParams.put("inspectionid", inspId+"");
+    rParams.put("inspectionid", inspId + "");
     rParams.put("MobileInd", "1");
     var report = aa.reportManager.getReportInfoModelByName("Cannabis Inspection Mobile Report");
     report = report.getOutput();
@@ -50,14 +48,14 @@ else
     report.setCapId(capId.getID1() + "-" + capId.getID2() + "-" + capId.getID3());
     report.setReportParameters(rParams);
     report.getEDMSEntityIdModel().setAltId(capId.getCustomID());
-    var permit = aa.reportManager.hasPermission("Cannabis Inspection Mobile Report",reportUser);
+    var permit = aa.reportManager.hasPermission("Cannabis Inspection Mobile Report", reportUser);
 
     if (permit.getOutput().booleanValue()) {
         var reportResult = aa.reportManager.getReportResult(report);
-        if(reportResult) {
+        if (reportResult) {
             reportOutput = reportResult.getOutput();
-            var reportFile=aa.reportManager.storeReportToDisk(reportOutput);
-            reportFile=reportFile.getOutput();
+            var reportFile = aa.reportManager.storeReportToDisk(reportOutput);
+            reportFile = reportFile.getOutput();
             rFiles.push(reportFile);
         }
     }
@@ -67,18 +65,18 @@ var contactResult = aa.people.getCapContactByCapID(capId);
 if (contactResult.getSuccess()) {
     var capContacts = contactResult.getOutput();
     for (var i in capContacts) {
-        if(matches(capContacts[i].getPeople().getContactType(),"Applicant","Authorized Agent")) {
+        if (matches(capContacts[i].getPeople().getContactType(), "Applicant", "Authorized Agent")) {
             var conName = getContactName(capContacts[i]);
-            var applicantEmail = capContacts[i].getPeople().getEmail()+"";
+            var applicantEmail = capContacts[i].getPeople().getEmail() + "";
             var inspectorName = getInspectorName(inspId);
-            if(!inspectorName)
+            if (!inspectorName)
                 inspectorName = "Inspector";
             var params = aa.util.newHashtable();
             addParameter(params, "$$InspectorOfRecord1$$", inspectorName);
             addParameter(params, "$$InspectorOfRecord2$$", inspectorName);
             addParameter(params, "$$InspectorPhoneNumber$$", getInspectorPhone(inspId));
             addParameter(params, "$$InspectorEmail$$", getInspectorEmail(inspId));
-            addParameter(params, "$$altId$$", capId.getCustomID()+"");
+            addParameter(params, "$$altId$$", capId.getCustomID() + "");
             addParameter(params, "$$InspectionStatus$$", inspResult);
             addParameter(params, "$$contactName$$", conName);
             addParameter(params, "$$InspectionType$$", inspType);
@@ -87,18 +85,18 @@ if (contactResult.getSuccess()) {
             var todayDate = (startDate.getMonth() + 1) + "/" + startDate.getDate() + "/" + startDate.getFullYear();
             addParameter(params, "$$date$$", todayDate);
             var parent = getParent();
-            if(parent)
-                addParameter(params, "$$parentAltId$$", parent.getCustomID()+"");
+            if (parent)
+                addParameter(params, "$$parentAltId$$", parent.getCustomID() + "");
             else
-                addParameter(params, "$$parentAltId$$", capId.getCustomID()+"");
-            addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptName"));
-            addParameter(params, "$$phoneHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","phoneHours"));
-            addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
-            addParameter(params, "$$officeHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","officeHours"));
-            addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
-            addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
-            addParameter(params, "$$deptEmail2$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail2"));
-            addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
+                addParameter(params, "$$parentAltId$$", capId.getCustomID() + "");
+            addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptName"));
+            addParameter(params, "$$phoneHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "phoneHours"));
+            addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptPhone"));
+            addParameter(params, "$$officeHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "officeHours"));
+            addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptHours"));
+            addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptEmail"));
+            addParameter(params, "$$deptEmail2$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptEmail2"));
+            addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptFormalName"));
             addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptAddress"));
             var COAs = "";
             var condResult = aa.capCondition.getCapConditions(capId);
@@ -110,33 +108,18 @@ if (contactResult.getSuccess()) {
                     var thisCond = aa.capCondition.getCapCondition(capId, cNbr).getOutput();
                     var cStatus = thisCond.getConditionStatus();
                     var isCOA = thisCond.getConditionOfApproval();
-                    if (matches(cStatus, "Applied","Pending") && isCOA == "Y") {
-                        COAs+= "  - "+thisCond.getConditionDescription();+", ";
+                    if (matches(cStatus, "Applied", "Pending") && isCOA == "Y") {
+                        COAs += "  - " + thisCond.getConditionDescription(); +", ";
                     }
                 }
             }
-            if(COAs!="")
+            if (COAs != "")
                 COAs = "The following documents are missing:\n" + COAs;
             addParameter(params, "$$COAs$$", COAs);
-            if(hm[applicantEmail+""] != 1 )
-            {
+            if (hm[applicantEmail + ""] != 1) {
                 sendEmail("no-reply@mendocinocounty.org", applicantEmail, "", "CAN_INSP_RESULT", params, VRFiles, capId);
-                hm[applicantEmail+""] = 1;
+                hm[applicantEmail + ""] = 1;
             }
-        }
-    }
-}
-
-// CAMEND-780
-if (appMatch("Cannabis/*/Application/NA")) {
-    if (inspType == "Cultivation - Site Inspection" || inspType == "Cultivation - Special Inspection" ||
-        inspType == "Nursery - Site Inspection" || inspType == "Nursery - Special Inspection") {
-        if (inspResult == "Pass" || inspResult == "Fail") {
-            addStdConditionX("Cannabis Required Document", "CCBL Affidavit");
-            // updateAppStatus("Closed","Closed automatically via Script", capId);
-            updateTask("Site Inspection", "Pass", "", "");
-            aa.workflow.adjustTask(capId, "Site Inspection", "N", "Y", null, null);
-            aa.workflow.adjustTask(capId, "Draft Decision", "Y", "N", null, null);
         }
     }
 }
