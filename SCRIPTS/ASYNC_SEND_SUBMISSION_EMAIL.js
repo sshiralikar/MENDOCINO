40a1e76edfda3a30d906b62baba841ee2a8051b7
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-02-04 09:00:48 -0500

    CAMEND-527

--- SCRIPTS/ASYNC_SEND_SUBMISSION_EMAIL.js
+++ SCRIPTS/ASYNC_SEND_SUBMISSION_EMAIL.js
@@ -147,43 +147,43 @@ try {
     }
 
     // CAMEND-527
-    // if (appTypeArray[2] == "Renewal") {
-    //     if (AInfo["Permit Type Change"] == "Yes") {
-    //         var hm = new Array();
-    //         var pCapId = getParent();
-    //         var capDetailObjResult = aa.cap.getCapDetail(pCapId); // Detail
-    //         if (capDetailObjResult.getSuccess()) {
-    //             var conName = "";
-    //             var contactResult = aa.people.getCapContactByCapID(pCapId);
-    //             if (contactResult.getSuccess()) {
-    //                 var capContacts = contactResult.getOutput();
-    //                 for (var i in capContacts) {
-    //                     conName = getContactName(capContacts[i]);
-    //                     var params = aa.util.newHashtable();
-    //                     addParameter(params, "$$altID$$", pCapId.getCustomID() + "");
-    //                     addParameter(params, "$$year$$", String(aa.date.getCurrentDate().getYear()));
-    //                     addParameter(params, "$$date$$", (new Date().getMonth() + 1) + "/" + new Date().getDate() + "/" + new Date().getFullYear());
-    //                     addParameter(params, "$$parentAltId$$", pCapId.getCustomID() + "");
-    //                     addParameter(params, "$$contactname$$", conName);
-    //                     addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptName"));
-    //                     addParameter(params, "$$phoneHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "phoneHours"));
-    //                     addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptPhone"));
-    //                     addParameter(params, "$$officeHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "officeHours"));
-    //                     addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptHours"));
-    //                     addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptEmail"));
-    //                     addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptAddress"));
-    //                     addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptFormalName"));
-    //                     addParameter(params, "$$FullNameBusName$$", conName);
-    //                     addParameter(params, "$$capAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias() + "");
-    //                     addParameter(params, "$$parentCapId$$", pCapId.getCustomID());
-    //                     addParameter(params, "$$Amendment$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias() + "");
-    //                     addParameter(params, "$$Location$$", getAddressInALine());
-    //                     sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "CAN_MODIFICATION REQUIRED", params, null, capId);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+    if (appTypeArray[2] == "Renewal") {
+        if (AInfo["Permit Type Change"] == "Yes") {
+            var hm = new Array();
+            var pCapId = getParent();
+            var capDetailObjResult = aa.cap.getCapDetail(pCapId); // Detail
+            if (capDetailObjResult.getSuccess()) {
+                var conName = "";
+                var contactResult = aa.people.getCapContactByCapID(pCapId);
+                if (contactResult.getSuccess()) {
+                    var capContacts = contactResult.getOutput();
+                    for (var i in capContacts) {
+                        conName = getContactName(capContacts[i]);
+                        var params = aa.util.newHashtable();
+                        addParameter(params, "$$altID$$", pCapId.getCustomID() + "");
+                        addParameter(params, "$$year$$", String(aa.date.getCurrentDate().getYear()));
+                        addParameter(params, "$$date$$", (new Date().getMonth() + 1) + "/" + new Date().getDate() + "/" + new Date().getFullYear());
+                        addParameter(params, "$$parentAltId$$", pCapId.getCustomID() + "");
+                        addParameter(params, "$$contactname$$", conName);
+                        addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptName"));
+                        addParameter(params, "$$phoneHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "phoneHours"));
+                        addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptPhone"));
+                        addParameter(params, "$$officeHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "officeHours"));
+                        addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptHours"));
+                        addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptEmail"));
+                        addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptAddress"));
+                        addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptFormalName"));
+                        addParameter(params, "$$FullNameBusName$$", conName);
+                        addParameter(params, "$$capAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias() + "");
+                        addParameter(params, "$$parentCapId$$", pCapId.getCustomID());
+                        addParameter(params, "$$Amendment$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias() + "");
+                        addParameter(params, "$$Location$$", getAddressInALine());
+                        sendEmail("no-reply@mendocinocounty.org", capContacts[i].getPeople().getEmail() + "", "", "CAN_MODIFICATION REQUIRED", params, null, capId);
+                    }
+                }
+            }
+        }
+    }
 }
 catch (err) {
     aa.sendMail("no-reply@mendocinocounty.gov", "sshiralikar@trustvip.com", "", "Error on Issuance Email ASYNC", err);
