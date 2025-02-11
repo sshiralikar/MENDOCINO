0b59eb96ab931dca0dbb787719640e8636d5edb3
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-02-11 09:21:54 -0500

    CAMEND-780

--- EVENT/IRSA;CANNABIS!~!RENEWAL!NA.js
+++ EVENT/IRSA;CANNABIS!~!RENEWAL!NA.js
@@ -1,10 +1,16 @@
 // CAMEND-780
 if (inspType == "Cultivation - Site Inspection" || inspType == "Cultivation - Special Inspection" ||
     inspType == "Nursery - Site Inspection" || inspType == "Nursery - Special Inspection") {
-    if (inspResult == "Pass" || inspResult == "Fail") {
+    if (inspResult == "Pass") {
         addStdConditionX("Cannabis Required Document", "CCBL Affidavit");
         // updateAppStatus("Closed","Closed automatically via Script", capId);
         updateTask("Inspection", "Pass", "", "");
+        aa.workflow.adjustTask(capId, "Inspection", "N", "Y", null, null);
+        aa.workflow.adjustTask(capId, "Issuance", "Y", "N", null, null);
+    } else if (inspResult == "Fail") {
+        addStdConditionX("Cannabis Required Document", "CCBL Affidavit");
+        // updateAppStatus("Closed","Closed automatically via Script", capId);
+        updateTask("Inspection", "Fail", "", "");
         aa.workflow.adjustTask(capId, "Inspection", "N", "Y", null, null);
         aa.workflow.adjustTask(capId, "Issuance", "Y", "N", null, null);
     }
