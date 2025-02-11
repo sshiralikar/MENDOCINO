c6581abd19dd91f6bf079077ef2e6670385b4661
sshiralikar <sshiralikar@trustvip.com>
2025-02-10 23:23:38 -0500

    CAMEND-794

--- EVENT/ASA;CANNABIS!CULTIVATION!APPLICATION!NA.js
+++ EVENT/ASA;CANNABIS!CULTIVATION!APPLICATION!NA.js
@@ -1,5 +1,5 @@
 //Add Fee
-if(!feeExists("CANCULT01","INVOICED","NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes")
+if(!feeExists("CANCULT01","INVOICED","NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes" && AInfo["Modification Approved for Conversion"]!="Yes")
     addFee("CANCULT01", "CAN_CULT", "FINAL", "1", "Y");
 //Add Fee
 
