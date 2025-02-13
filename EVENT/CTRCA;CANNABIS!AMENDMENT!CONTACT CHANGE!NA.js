c52c67567d56b8dfb26e0dcad2bad0d1bfd74bc2
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-02-13 10:25:41 -0500

    CAMEND-846

--- EVENT/CTRCA;CANNABIS!AMENDMENT!CONTACT CHANGE!NA.js
+++ EVENT/CTRCA;CANNABIS!AMENDMENT!CONTACT CHANGE!NA.js
@@ -1,6 +1,11 @@
 //CAMEND-846
+updateTask("Amendment Review", "Approved", "", "");
+aa.workflow.adjustTask(capId, "Amendment Review", "N", "Y", null, null);
+updateAppStatus("Approved","",capId);
+
 var licCapId = getParent();
-copyContactsFromContTypeToContType(capId, licCapId, "Authorized Agent", "Property Owner");
+copyContactsFromContTypeToContType(capId, licCapId, "Authorized Agent", "Authorized Agent");
+copyContactsFromContTypeToContType(capId, licCapId, "Property Owner", "Property Owner");
 
 var capCondResult = aa.capCondition.getCapConditions(capId);
 if (capCondResult.getSuccess()) {
@@ -15,49 +20,49 @@ if (capCondResult.getSuccess()) {
 }
 
 function copyContactsFromContTypeToContType(srcCapId, targetCapId, pContactType, pToContactType) {
-	try {
-		logDebug("copyContactsFromContTypeToContType() started");
-		var capPeoples = getPeople(srcCapId);
-		if (capPeoples == null || capPeoples.length == 0) {
-			return;
-		}
-		var targetPeople = getPeople(targetCapId);
-		for (loopk in capPeoples) {
-			var sourcePeopleModel = capPeoples[loopk];
-			sourcePeopleModel.getCapContactModel().setCapID(targetCapId);
-			var targetPeopleModel = null;
-			var doSkip = true;
-			var vContactTypeTemp = sourcePeopleModel.getCapContactModel().getContactType();
-			logDebug("vContactTypeTemp:" + vContactTypeTemp);
-			if (pContactType != null && pContactType == vContactTypeTemp) {
-				if (pToContactType != null) {
-					sourcePeopleModel.getCapContactModel().setContactType(pToContactType);
-				}
-				doSkip = false;
-			}
-			if (!doSkip) {
-				if (targetPeople != null && targetPeople.length > 0) {
-					for (loop2 in targetPeople) {
-						if (isMatchPeople(sourcePeopleModel, targetPeople[loop2])) {
-							targetPeopleModel = targetPeople[loop2];
-							break;
-						}
-					}
-				}
-				if (targetPeopleModel != null) {
-					aa.people.copyCapContactModel(sourcePeopleModel.getCapContactModel(), targetPeopleModel.getCapContactModel());
-					if (targetPeopleModel.getCapContactModel().getPeople() != null && sourcePeopleModel.getCapContactModel().getPeople()) {
-						targetPeopleModel.getCapContactModel().getPeople().setContactAddressList(sourcePeopleModel.getCapContactModel().getPeople().getContactAddressList());
-					}
-					aa.people.editCapContactWithAttribute(targetPeopleModel.getCapContactModel());
-				} else {
-					aa.people.createCapContactWithAttribute(sourcePeopleModel.getCapContactModel());
-				}
-			}
-		}
-	} catch (err) {
-		logDebug("**Error: copyContactsFromContTypeToContType()" + err.message);
-	}
+    try {
+        logDebug("copyContactsFromContTypeToContType() started");
+        var capPeoples = getPeople(srcCapId);
+        if (capPeoples == null || capPeoples.length == 0) {
+            return;
+        } copyContactsFromContTypeToContType
+        var targetPeople = getPeople(targetCapId);
+        for (loopk in capPeoples) {
+            var sourcePeopleModel = capPeoples[loopk];
+            sourcePeopleModel.getCapContactModel().setCapID(targetCapId);
+            var targetPeopleModel = null;
+            var doSkip = true;
+            var vContactTypeTemp = sourcePeopleModel.getCapContactModel().getContactType();
+            logDebug("vContactTypeTemp:" + vContactTypeTemp);
+            if (pContactType != null && pContactType == vContactTypeTemp) {
+                if (pToContactType != null) {
+                    sourcePeopleModel.getCapContactModel().setContactType(pToContactType);
+                }
+                doSkip = false;
+            }
+            if (!doSkip) {
+                if (targetPeople != null && targetPeople.length > 0) {
+                    for (loop2 in targetPeople) {
+                        if (isMatchPeople(sourcePeopleModel, targetPeople[loop2])) {
+                            targetPeopleModel = targetPeople[loop2];
+                            break;
+                        }
+                    }
+                }
+                if (targetPeopleModel != null) {
+                    aa.people.copyCapContactModel(sourcePeopleModel.getCapContactModel(), targetPeopleModel.getCapContactModel());
+                    if (targetPeopleModel.getCapContactModel().getPeople() != null && sourcePeopleModel.getCapContactModel().getPeople()) {
+                        targetPeopleModel.getCapContactModel().getPeople().setContactAddressList(sourcePeopleModel.getCapContactModel().getPeople().getContactAddressList());
+                    }
+                    aa.people.editCapContactWithAttribute(targetPeopleModel.getCapContactModel());
+                } else {
+                    aa.people.createCapContactWithAttribute(sourcePeopleModel.getCapContactModel());
+                }
+            }
+        }
+    } catch (err) {
+        logDebug("**Error: copyContactsFromContTypeToContType()" + err.message);
+    }
 }
 
 function getPeople(capId) {
