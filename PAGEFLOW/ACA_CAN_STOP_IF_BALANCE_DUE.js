f4c12c18af85b7b65a22a0b5d53bd12c2488e8f4
sshiralikar <sshiralikar@trustvip.com>
2025-02-17 22:05:55 -0500

    CAMEND-850

--- PAGEFLOW/ACA_CAN_STOP_IF_BALANCE_DUE.js
+++ PAGEFLOW/ACA_CAN_STOP_IF_BALANCE_DUE.js
@@ -152,11 +152,11 @@ try {
         var vCap = aa.cap.getCap(vCapID).getOutput();
         if(vCap.isCompleteCap() && (vCapID+"" != capId+""))
         {
-            var balanceDue = getBalanceDue(vCapID,0);
+            /*var balanceDue = getBalanceDue(vCapID,0);
             if (balanceDue > 0) {
                 records = true;
                 balRecords += "<p>"+vCapID.customID+"; </p>";
-            }
+            }*/
             if((vCap.getCapStatus() == "Terminated" || vCap.getCapStatus() == "Terminated - No Apply") && String(vCap.getCapType()).indexOf("Permit")!=-1){
                 records = true;
                 terRecords += "<p>"+vCapID.customID+"; </p>";
@@ -168,8 +168,8 @@ try {
         cancel = true;
         showMessage = true;
         message+="You either have an existing balance due on a previous application or have record of being terminated from the Mendocino Cannabis Program. Please contact the department for any inquiries. Thank you.<p></p><hr>";
-        if(balRecords!="")
-            message+="Records with balance due: "+ balRecords;
+        //if(balRecords!="")
+        //    message+="Records with balance due: "+ balRecords;
         if(terRecords!="")
             message+="Terminated Records : "+ terRecords;
     }
