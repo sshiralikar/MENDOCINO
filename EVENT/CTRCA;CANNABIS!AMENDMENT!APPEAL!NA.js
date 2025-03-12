aad35ee57bcd8b99a4198b4fbdfd50f7effb8377
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-03-12 16:54:36 -0400

    CAMEND-852

--- EVENT/CTRCA;CANNABIS!AMENDMENT!APPEAL!NA.js
+++ EVENT/CTRCA;CANNABIS!AMENDMENT!APPEAL!NA.js
@@ -5,10 +5,8 @@ if (parentCapId != null && parentCapId != "")
 logDebug("sysDateMMDDYYYY: " + sysDateMMDDYYYY);
 editAppSpecific("Appeal Submitted Date", sysDateMMDDYYYY, licCapId);
 
-// // CAMEND-852
-// if (!feeExists("CANAPPL1", "INVOICED", "NEW") && AInfo["Paying in Person"] == "No") {
-//     addFee("CANAPPL1", "CAN_APPEAL", "FINAL", "1", "Y");
-// } else if (!feeExists("CANAPPL1") && AInfo["Paying in Person"] == "Yes") {
-//     addFee("CANAPPL1", "CAN_APPEAL", "FINAL", "1", "N");
-//     addStdConditionX("General", "Pay in Person");
-// }
+// CAMEND-852
+if (!feeExists("CANAPPL1") && AInfo["Paying in Person"] == "Yes") {
+    addFee("CANAPPL1", "CAN_APPEAL", "FINAL", "1", "N");
+    addStdConditionX("General", "Pay in Person");
+}
