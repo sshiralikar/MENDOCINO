ad513bdd5ddfe2def734fe6ed717ecffbdc2fed5
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-03-12 16:37:52 -0400

    CAMEND-852

--- EVENT/ASA;CANNABIS!AMENDMENT!APPEAL!NA.js
+++ EVENT/ASA;CANNABIS!AMENDMENT!APPEAL!NA.js
@@ -4,4 +4,12 @@ if (!publicUser) {
     if (parentCapId != null && parentCapId != "")
         licCapId = parentCapId;
     editAppSpecific("Appeal Submitted Date", sysDateMMDDYYYY, licCapId);
+
+    // CAMEND-852
+    if (!feeExists("CANAPPL1", "INVOICED", "NEW") && AInfo["Paying in Person"] == "No") {
+        addFee("CANAPPL1", "CAN_APPEAL", "FINAL", "1", "Y");
+    } else if (!feeExists("CANAPPL1") && AInfo["Paying in Person"] == "Yes") {
+        addFee("CANAPPL1", "CAN_APPEAL", "FINAL", "1", "N");
+        addStdConditionX("General", "Pay in Person");
+    }
 }
--- EVENT/CTRCA;CANNABIS!AMENDMENT!APPEAL!NA.js
+++ EVENT/CTRCA;CANNABIS!AMENDMENT!APPEAL!NA.js
@@ -4,3 +4,11 @@ if (parentCapId != null && parentCapId != "")
     licCapId = parentCapId;
 logDebug("sysDateMMDDYYYY: " + sysDateMMDDYYYY);
 editAppSpecific("Appeal Submitted Date", sysDateMMDDYYYY, licCapId);
+
+// CAMEND-852
+if (!feeExists("CANAPPL1", "INVOICED", "NEW") && AInfo["Paying in Person"] == "No") {
+    addFee("CANAPPL1", "CAN_APPEAL", "FINAL", "1", "Y");
+} else if (!feeExists("CANAPPL1") && AInfo["Paying in Person"] == "Yes") {
+    addFee("CANAPPL1", "CAN_APPEAL", "FINAL", "1", "N");
+    addStdConditionX("General", "Pay in Person");
+}
