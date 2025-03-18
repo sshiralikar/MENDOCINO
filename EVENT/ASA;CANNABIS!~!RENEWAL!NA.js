82db2f70f48f282fa69bbcde25b9133ba7f2bae8
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-03-18 12:56:27 -0400

    CAMEND-852

--- EVENT/ASA;CANNABIS!~!RENEWAL!NA.js
+++ EVENT/ASA;CANNABIS!~!RENEWAL!NA.js
@@ -9,46 +9,51 @@ vGoodToRenew = prepareRenewal();
 if (parentCapId != null && vGoodToRenew) {
 
     //Copy Parcels from license to renewal
-    copyParcels(parentCapId,capId);
+    copyParcels(parentCapId, capId);
 
     //Copy addresses from license to renewal
-    copyAddresses(parentCapId,capId);
+    copyAddresses(parentCapId, capId);
 
     //Copy addresses from license to renewal
-    copyOwner(parentCapId,capId);
+    copyOwner(parentCapId, capId);
 
     //copy ASI Info from license to renewal
     //copyASIFields(parentCapId,capId);
     copyMatchingCustomFields(parentCapId, capId, true)
 
     //Copy ASIT from license to renewal
-    copyASITables(parentCapId,capId);
+    copyASITables(parentCapId, capId);
 
     //Copy Contacts from license to renewal
-    copyContacts(parentCapId,capId);
+    copyContacts(parentCapId, capId);
 
     copyLicensedProf(parentCapId, capId);
 
     copyConditions(parentCapId, capId);
 
     //Copy Work Description from license to renewal
-    aa.cap.copyCapWorkDesInfo(parentCapId,capId);
+    aa.cap.copyCapWorkDesInfo(parentCapId, capId);
 
     //Copy application name from license to renewal
-    editAppName(getAppName(parentCapId),capId);
+    editAppName(getAppName(parentCapId), capId);
 }
 //End Core Renewal Functionality*/
-if(!publicUser)
-{
-    updateAppStatus("Pending","Updated through script");
-    if(parentCapId)
-        updateAppStatus("Renewal Under Review","Updated through script",parentCapId);
-    if(AInfo["Structure Change"] == "Yes")
-    {
-        taskCloseAllExcept("Modification Required","Closing via script");
-        updateAppStatus("Modification Required","Updating via Script");
-        updateAppStatus("Modification Required","Updating via Script",parentCapId);
+if (!publicUser) {
+    updateAppStatus("Pending", "Updated through script");
+    if (parentCapId)
+        updateAppStatus("Renewal Under Review", "Updated through script", parentCapId);
+    if (AInfo["Structure Change"] == "Yes") {
+        taskCloseAllExcept("Modification Required", "Closing via script");
+        updateAppStatus("Modification Required", "Updating via Script");
+        updateAppStatus("Modification Required", "Updating via Script", parentCapId);
+    }
+    if (!feeExists("CANREN01", "INVOICED", "NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes") {
+        addFee("CANREN01", "CAN_REN", "FINAL", "1", "Y");
     }
 }
-if(!feeExists("CANREN01","INVOICED","NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes")
-    addFee("CANREN01", "CAN_REN", "FINAL", "1", "Y");
+// CAMEND-852
+if (AInfo["Paying in Person"] == "No") {
+    if (!feeExists("CANREN01", "INVOICED", "NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes") {
+        addFee("CANREN01", "CAN_REN", "FINAL", "1", "Y");
+    }
+}
--- EVENT/CTRCA;CANNABIS!~!RENEWAL!~.js
+++ EVENT/CTRCA;CANNABIS!~!RENEWAL!~.js
@@ -6,3 +6,10 @@ if (AInfo["Structure Change"] == "Yes") {
     updateAppStatus("Modification Required", "Updating via Script");
     updateAppStatus("Modification Required", "Updating via Script", parentCapId);
 }
+// CAMEND-852
+if (AInfo["Paying in Person"] == "Yes") {
+    if (!feeExists("CANREN01", "INVOICED", "NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes") {
+        addFee("CANREN01", "CAN_REN", "FINAL", "1", "Y");
+        addStdConditionX("General", "Pay in Person");
+    }
+}
