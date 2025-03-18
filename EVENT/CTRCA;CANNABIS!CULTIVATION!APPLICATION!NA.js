314e09ba815d75ed7f797b9dc1eabea7aa19b598
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-03-18 12:39:31 -0400

    CAMEND-852

--- EVENT/CTRCA;CANNABIS!CULTIVATION!APPLICATION!NA.js
+++ EVENT/CTRCA;CANNABIS!CULTIVATION!APPLICATION!NA.js
@@ -2,6 +2,6 @@
 if (AInfo["Paying in Person"] == "Yes") {
     if (!feeExists("CANCULT01") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes" && AInfo["Modification Approved for Conversion"] != "Yes") {
         addFee("CANCULT01", "CAN_CULT", "FINAL", "1", "Y");
-    addStdConditionX("General", "Pay in Person");
+        addStdConditionX("General", "Pay in Person");
     }
 }
