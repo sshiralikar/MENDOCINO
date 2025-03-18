4b141e19ff5a49fc85d0166d940a46f59b9c0018
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-03-18 13:03:22 -0400

    CAMEND-852

--- EVENT/ASA;CANNABIS!AMENDMENT!MODIFICATION!NA.js
+++ EVENT/ASA;CANNABIS!AMENDMENT!MODIFICATION!NA.js
@@ -1,6 +1,13 @@
 if(!publicUser)
 {
     updateAppStatus("Modification Under Review","Updated through script",parentCapId);
+    if(!feeExists("CANMOD01","INVOICED","NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes") {
+        addFee("CANMOD01", "CAN_MOD", "FINAL", "1", "Y");
+    }
 }
-if(!feeExists("CANMOD01","INVOICED","NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes")
-    addFee("CANMOD01", "CAN_MOD", "FINAL", "1", "Y");
+// CAMEND-852
+if (AInfo["Paying in Person"] == "No") {
+    if(!feeExists("CANMOD01","INVOICED","NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes") {
+        addFee("CANMOD01", "CAN_MOD", "FINAL", "1", "Y");
+    }
+}
--- EVENT/CTRCA;CANNABIS!AMENDMENT!MODIFICATION!NA.js
+++ EVENT/CTRCA;CANNABIS!AMENDMENT!MODIFICATION!NA.js
@@ -1,1 +1,9 @@
 updateAppStatus("Modification Under Review","Updated through script",parentCapId);
+
+// CAMEND-852
+if (AInfo["Paying in Person"] == "Yes") {
+    if(!feeExists("CANMOD01","INVOICED","NEW") && AInfo["Equity Eligibility"] != "Yes" && AInfo["Exempt"] != "Yes") {
+        addFee("CANMOD01", "CAN_MOD", "FINAL", "1", "Y");
+        addStdConditionX("General", "Pay in Person");
+    }
+}
