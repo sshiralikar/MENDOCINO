4ecdc78492178a1b772e7581be1d1bea25fd775b
Edward <141940007+ehamidovVIP@users.noreply.github.com>
2025-06-16 16:06:52 -0400

    CAMEND-654

--- EVENT/WTUA;CANNABIS!~!APPLICATION!NA.js
+++ EVENT/WTUA;CANNABIS!~!APPLICATION!NA.js
@@ -428,15 +428,25 @@ if(wfStatus == "Denied")
             for (var i in capContacts) {
                 conName = getContactName(capContacts[i]);
                 var params = aa.util.newHashtable();
-                addParameter(params, "$$altID$$", capId.getCustomID()+"");
+                // CAMEND-654
+                var parent = getParent();
+                if (parent) {
+                    addParameter(params, "$$parentAltId$$", parent.getCustomID() + "");
+                }
+                else {
+                    addParameter(params, "$$parentAltId$$", capId.getCustomID() + "");
+                }
+                addParameter(params, "$$date$$", sysDateMMDDYYYY);
+                addParameter(params, "$$altID$$", capId.getCustomID() + "");
                 addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias()+"");
                 addParameter(params, "$$capName$$", capName);
                 addParameter(params, "$$deptName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptName"));
                 addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
                 addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
                 addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
+                addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptAddress"));
                 addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
-                addParameter(params, "$$contactname$$", conName);
+                addParameter(params, "$$contactName$$", conName);
                 addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
                 addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
                 if(hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
@@ -461,6 +471,15 @@ if(wfStatus == "Appeal Denied")
         for (var i in capContacts) {
             conName = getContactName(capContacts[i]);
             var params = aa.util.newHashtable();
+            // CAMEND-654
+            var parent = getParent();
+            if (parent) {
+                addParameter(params, "$$parentAltId$$", parent.getCustomID() + "");
+            }
+            else {
+                addParameter(params, "$$parentAltId$$", capId.getCustomID() + "");
+            }
+            addParameter(params, "$$date$$", sysDateMMDDYYYY);
             addParameter(params, "$$altID$$", capId.getCustomID()+"");
             addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias()+"");
             addParameter(params, "$$capName$$", capName);
@@ -468,8 +487,9 @@ if(wfStatus == "Appeal Denied")
             addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptPhone"));
             addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptHours"));
             addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptEmail"));
+            addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptAddress"));
             addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS","deptFormalName"));
-            addParameter(params, "$$contactname$$", conName);
+            addParameter(params, "$$contactName$$", conName);
             addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
             addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
             if(hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
--- EVENT/WTUA;CANNABIS!~!RENEWAL!NA.js
+++ EVENT/WTUA;CANNABIS!~!RENEWAL!NA.js
@@ -271,6 +271,15 @@ if (wfTask == "Issuance" && wfStatus == "Denied") {
                 if (matches(capContacts[i].getPeople().getContactType(), "Applicant", "Authorized Agent")) {
                     conName = getContactName(capContacts[i]);
                     var params = aa.util.newHashtable();
+                    // CAMEND-654
+                    var parent = getParent();
+                    if (parent) {
+                        addParameter(params, "$$parentAltId$$", parent.getCustomID() + "");
+                    }
+                    else {
+                        addParameter(params, "$$parentAltId$$", capId.getCustomID() + "");
+                    }
+                    addParameter(params, "$$date$$", sysDateMMDDYYYY);
                     addParameter(params, "$$altID$$", capId.getCustomID() + "");
                     addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(capId).getOutput().getCapType().getAlias() + "");
                     addParameter(params, "$$capName$$", capName);
@@ -278,8 +287,9 @@ if (wfTask == "Issuance" && wfStatus == "Denied") {
                     addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptPhone"));
                     addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptHours"));
                     addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptEmail"));
+                    addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptAddress"));
                     addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptFormalName"));
-                    addParameter(params, "$$contactname$$", conName);
+                    addParameter(params, "$$contactName$$", conName);
                     addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
                     addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
                     if (hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
@@ -331,6 +341,15 @@ if (wfTask == "Draft Decision" && wfStatus == "Denied") {
             if (matches(capContacts[i].getPeople().getContactType(), "Applicant", "Authorized Agent")) {
                 conName = getContactName(capContacts[i]);
                 var params = aa.util.newHashtable();
+                // CAMEND-654
+                var parent = getParent();
+                if (parent) {
+                    addParameter(params, "$$parentAltId$$", parent.getCustomID() + "");
+                }
+                else {
+                    addParameter(params, "$$parentAltId$$", capId.getCustomID() + "");
+                }
+                addParameter(params, "$$date$$", sysDateMMDDYYYY);
                 addParameter(params, "$$altID$$", capId.getCustomID() + "");
                 addParameter(params, "$$capTypeAlias$$", aa.cap.getCap(licCapId).getOutput().getCapType().getAlias() + "");
                 addParameter(params, "$$capName$$", capName);
@@ -338,8 +357,9 @@ if (wfTask == "Draft Decision" && wfStatus == "Denied") {
                 addParameter(params, "$$deptPhone$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptPhone"));
                 addParameter(params, "$$deptHours$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptHours"));
                 addParameter(params, "$$deptEmail$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptEmail"));
+                addParameter(params, "$$deptAddress$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptAddress"));
                 addParameter(params, "$$deptFormalName$$", lookup("NOTIFICATION_TEMPLATE_INFO_CANNABIS", "deptFormalName"));
-                addParameter(params, "$$contactname$$", conName);
+                addParameter(params, "$$contactName$$", conName);
                 addParameter(params, "$$ACAUrl$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
                 addParameter(params, "$$ACAURL$$", String(lookup("ACA_CONFIGS", "ACA_SITE")).split("/Admin")[0]);
                 if (hm[capContacts[i].getPeople().getEmail() + ""] != 1) {
