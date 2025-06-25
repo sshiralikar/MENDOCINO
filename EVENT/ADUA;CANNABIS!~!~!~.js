eff4c67f60b0f31555aaf0f6359163a45d9a4217
sshiralikar <sshiralikar@trustvip.com>
2025-06-24 21:16:12 -0400

    dateFormatted

--- EVENT/ADUA;CANNABIS!~!~!~.js
+++ EVENT/ADUA;CANNABIS!~!~!~.js
@@ -1,3 +1,17 @@
 //Populate Geographic Information
 include("POPULATE_GEOGRAPHIC_INFORMATION");
 //Populate Geographic Information
+
+var vWFTasks = aa.workflow.getTasks(capId).getOutput();
+for (var vCounter in vWFTasks)
+{
+    var vWFTask = vWFTasks[vCounter];
+    if (vWFTask.completeFlag == "Y")
+        continue;
+    var vTaskDesc = vWFTask.taskDescription;
+    var vAssignedStaff = vWFTask.getAssignedStaff();
+    var vAssignedDept = vAssignedStaff.deptOfUser;
+    var applicUserId = aa.person.getUser(vAssignedStaff.getFirstName(), vAssignedStaff.getMiddleName(), vAssignedStaff.getLastName()).getOutput();
+    var finalUser = applicUserId.getUserID();
+    assignTask(vTaskDesc,finalUser);
+}
