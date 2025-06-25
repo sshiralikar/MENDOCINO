939862a38b34689cf60a0a55f654ef46f64d4f1d
sshiralikar <sshiralikar@trustvip.com>
2025-06-24 21:22:07 -0400

    dateFormatted

--- EVENT/ADUA;CANNABIS!~!~!~.js
+++ EVENT/ADUA;CANNABIS!~!~!~.js
@@ -2,16 +2,22 @@
 include("POPULATE_GEOGRAPHIC_INFORMATION");
 //Populate Geographic Information
 
-var vWFTasks = aa.workflow.getTasks(capId).getOutput();
-for (var vCounter in vWFTasks)
-{
-    var vWFTask = vWFTasks[vCounter];
-    if (vWFTask.completeFlag == "Y")
-        continue;
-    var vTaskDesc = vWFTask.taskDescription;
-    var vAssignedStaff = vWFTask.getAssignedStaff();
-    var vAssignedDept = vAssignedStaff.deptOfUser;
-    var applicUserId = aa.person.getUser(vAssignedStaff.getFirstName(), vAssignedStaff.getMiddleName(), vAssignedStaff.getLastName()).getOutput();
-    var finalUser = applicUserId.getUserID();
-    assignTask(vTaskDesc,finalUser);
+
+var capDetailObjResult = aa.cap.getCapDetail(capId);
+
+if (capDetailObjResult.getSuccess()) {
+    capDetail = capDetailObjResult.getOutput();
+    capDetail.setAsgnStaff(userID);
+    var assignedID = capDetail.getAsgnStaff();
+
+    var vWFTasks = aa.workflow.getTasks(capId).getOutput();
+    for (var vCounter in vWFTasks)
+    {
+        var vWFTask = vWFTasks[vCounter];
+        if (vWFTask.completeFlag == "Y")
+            continue;
+        var vTaskDesc = vWFTask.taskDescription;
+
+        assignTask(vTaskDesc,assignedID);
+    }
 }
